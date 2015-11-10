92e3ecae3bbda995f29351e1948becebd885d4d8
glennlopez <glennlopez@gmail.com>
2015-11-10 12:35:41 -0800

    update

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	/* code */
+	cout << "hello world\n";
+	cout << "this is a test\n";
+	cout << endl;
+	return 0;
+}
+
+/* Notes */
+// compile using: g++ hello.cpp -o test.bin
+// run using: ./test.bin
