a0632cdec048a4b433ed56482c8afed9cd4b5bdb
glennlopez <glennlopez@gmail.com>
2015-11-16 17:13:43 -0800

    update

---
+++ zyBooks/read_write_string.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+	A user types a word and a number. Read them into the provided variables.
+	Then print: word_number. End with newline. Example output if user entered:
+	Amy 5
+					Amy_5 
+*/
+
+int main() {
+   string userWord;
+   int userNum = 0;
+
+   /* Your solution goes here  */
+
+   return 0;
+}
