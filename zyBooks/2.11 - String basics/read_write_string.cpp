23f9dc5d8c94aa60be8c19d27034be1a18269581
glennlopez <glennlopez@gmail.com>
2015-11-16 17:54:26 -0800

    update

--- zyBooks/2.11 - String basics/read_write_string.cpp
+++ zyBooks/2.11 - String basics/read_write_string.cpp
@@ -6,7 +6,7 @@ using namespace std;
 	A user types a word and a number. Read them into the provided variables.
 	Then print: word_number. End with newline. Example output if user entered:
 	Amy 5
-					Amy_5 
+					Amy_5
 */
 
 int main() {
@@ -14,6 +14,13 @@ int main() {
    int userNum = 0;
 
    /* Your solution goes here  */
+	//cout << "Type word: "; <--- dont need
+	cin >> userWord;
+
+	//cout << "Type number: "; <--- dont need
+	cin >> userNum;
+
+	cout << userWord << "_" << userNum << endl;
 
    return 0;
 }
