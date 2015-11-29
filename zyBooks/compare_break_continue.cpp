d67430ef997b7414d7c27c64e3c9158323e7de79
glennlopez <glennlopez@gmail.com>
2015-11-28 22:25:53 -0800

    added functions

--- zyBooks/compare_break_continue.cpp
+++ zyBooks/compare_break_continue.cpp
@@ -2,9 +2,36 @@
 using namespace std;
 
 //prototypes
+void Regular();
+void Break();
+void Continue();
 
 int main(int argc, char const *argv[]) {
 	//un-comment desired functions to compare
 
+	//Regular();
+	//Break();
+	//Continue();
+
 	return 0;
+}
+
+
+/**************
+	FUNCTIONS
+***************/
+
+//regular loop
+void Regular(){
+
+}
+
+//break loop
+void Break(){
+
+}
+
+//continue loop
+void Continue(){
+
 }
