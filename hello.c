8947929de5506585ebbb977b7558cd6e297d5cdc
glennlopez <glennlopez@gmail.com>
2015-11-28 19:38:57 -0800

    folder cleanup

--- hello.c
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
-
-int main(int argc, char const *argv[]) {
-	/* code */
-	return 0;
-}
--- zyBooks/understanding_continue.cpp
+++ zyBooks/understanding_continue.cpp
@@ -3,5 +3,12 @@ using namespace std;
 
 int main(int argc, char const *argv[]) {
 	/* code */
+
+	int i = 0;
+	int j = 0;
+
+
+
+
 	return 0;
 }
