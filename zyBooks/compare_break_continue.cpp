610498c62fbfaedd1fe0c9eff52419be00bb53e4
glennlopez <glennlopez@gmail.com>
2015-11-28 22:53:28 -0800

    created functions

--- zyBooks/compare_break_continue.cpp
+++ zyBooks/compare_break_continue.cpp
@@ -23,15 +23,42 @@ int main(int argc, char const *argv[]) {
 
 //regular loop
 void Regular(){
-
+	cout << "------ Regular Loop \n";
+	for (int i = 0; i < 5; i++){
+		cout << "i = " << i << endl;
+		// break or continue
+		cout << "Do some more stuff with " << i << endl;
+	}
+	cout << "\n\n";
 }
 
 //break loop
 void Break(){
+	cout << "------ Break Loop \n";
+	for (int i = 0; i < 5; i++){
+		cout << "i = " << i << endl;
+		// break or continue
+		if (i == 2){
+			break;
+		}
+		cout << "Do some more stuff with " << i << endl;
+	}
+	cout << "\n\n";
 
 }
 
 //continue loop
 void Continue(){
+	cout << "------ Continue Loop \n";
+	for (int i = 0; i < 5; i++){
+		cout << "i = " << i << endl;
+		// break or continue
+		if (i == 2){
+			continue;
+		}
+		cout << "Do some more stuff with " << i << endl;
+	}
+	cout << "\n\n";
+
 
 }
