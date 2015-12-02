aa04c20f1efd7799f9a934ec8142de360bb1fe19
glennlopez <glennlopez@gmail.com>
2015-12-02 15:36:59 -0800

    fixed maxVal zero bug

--- zyBooks/fidMax_tryit.cpp
+++ zyBooks/fidMax_tryit.cpp
@@ -11,12 +11,19 @@ int main(int argc, char const *argv[]) {
 	int maxVal = 0;
 
 	cout << "Enter 8 integer values..." << endl;
+
+	//iterate input loop
 	for (int i = 0; i < NUM_VALS; i++){
 		cout << "Value (" << i << "): ";
 		cin >> usrValues.at(i);
+	}
 
-		if (usrValues.at(i) > maxVal){
-			maxVal = usrValues.at(i);
+	maxVal = usrValues.at(0); //set maxVal so its NOT zero 0
+
+	//check for max val outside of input loop
+	for (int j = 0; j < NUM_VALS; j++){
+		if (usrValues.at(j) > maxVal){
+			maxVal = usrValues.at(j);
 		}
 	}
 
