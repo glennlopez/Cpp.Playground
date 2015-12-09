614fadf6d7a67e7e4ef1b22cb6309b8cadd74073
glennlopez <glennlopez@gmail.com>
2015-12-08 20:20:47 -0800

    conver to 0 and positive

--- zyBooks/convertZero_tryit.cpp
+++ zyBooks/convertZero_tryit.cpp
@@ -4,14 +4,44 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
+	//5 vector elements
+	const int NUM_ELEMENTS = 5;
+	vector<int> userNum(NUM_ELEMENTS);
+
+	//loop vars
+	int i = 0;
+
 
 	//ask for numbers (input)
+	for (i = 0; i < NUM_ELEMENTS; i++){
+		cout << "Numbers to store: ";
+		cin >> userNum.at(i);
+	}
 
 
 	//conver any negative values to a zero
+	/*
+	for (i = 0; i < NUM_ELEMENTS; i++){
+		if (userNum.at(i) < 0){
+			userNum.at(i) = 0;
+		}
+	}
+	*/
+
+	//convert any negative numbers to postitive
+	for (i = 0; i < NUM_ELEMENTS; i++){
+		if (userNum.at(i) < 0){
+			userNum.at(i) = userNum.at(i) * -1;
+		}
+	}
+
 
 
 	//output the converted numbers
+	for (i = 0; i < NUM_ELEMENTS; i++){
+		cout << userNum.at(i) << " ";
+	}
 
+	cout << endl;
 	return 0;
 }
