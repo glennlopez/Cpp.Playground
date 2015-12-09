ea75bb0a8acd45ff8b666fae1142302630fd4665
glennlopez <glennlopez@gmail.com>
2015-12-08 19:35:48 -0800

    template done

---
+++ zyBooks/convertZero_tryit.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+	//ask for numbers (input)
+
+
+	//conver any negative values to a zero
+
+
+	//output the converted numbers
+
+	return 0;
+}
