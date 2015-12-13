ecbf044fb24b2f76f17ce909ec16b86f6c13612e
glennlopez <glennlopez@gmail.com>
2015-12-13 00:56:59 -0600

    update

--- zyBooks/tictactoe.cpp
+++ zyBooks/tictactoe.cpp
@@ -9,14 +9,19 @@ using namespace std;
 		x!x!x
 		~~~~~
 		x!x!x
-	*/
-	
+*/
 
 void PrintTicTacToe(char horizChar, char vertChar) {
 
    /* Your solution goes here  */
+	cout << 'x' << vertChar << 'x' << vertChar << 'x' << endl;
+	cout << horizChar << horizChar << horizChar << horizChar << horizChar << endl;
+	cout << 'x' << vertChar << 'x' << vertChar << 'x' << endl;
+	cout << horizChar << horizChar << horizChar << horizChar << horizChar << endl;
+	cout << 'x' << vertChar << 'x' << vertChar << 'x' << endl;
 
    return;
+}
 
 int main() {
    PrintTicTacToe('~', '!');
