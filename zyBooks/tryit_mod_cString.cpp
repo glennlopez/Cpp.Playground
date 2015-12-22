11412ea59e99523f6de70956d2ff0eb47a5d082e
glennlopez <glennlopez@gmail.com>
2015-12-22 02:16:21 -0800

    update

---
+++ zyBooks/tryit_mod_cString.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+
+int main(int argc, char const *argv[]) {
+	//variables
+
+	//ask for user input using cstring
+
+	//convert all spaces to hyphens
+	convertHyp(); //arguments (cstring& cstring)
+
+	//output user input using cstring
+
+	return 0;
+}
