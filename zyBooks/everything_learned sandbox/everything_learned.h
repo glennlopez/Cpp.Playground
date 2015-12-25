4ba4dc2d0f7530c5720f33cef8f8f51038349002
glennlopez <glennlopez@gmail.com>
2015-12-25 05:26:05 -0800

    update

---
+++ zyBooks/everything_learned sandbox/everything_learned.h
@@ -0,0 +1,16 @@
+#ifndef EVERYTHING_LEARNED_H
+#define EVERYTHING_LEARNED_H
+/* start header file */
+
+//definitions
+#define DEFAULT_INT 0;
+#define DEFAULT_CHAR '?';
+#define DEFAULT 0;
+
+//prototypes
+void YesOrNo(char& errorYN);		//checks for 'y' or 'n' input
+
+
+
+/* end of header file */
+#endif
