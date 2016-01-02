09dffa7991348d8de15c528128a9ae56d6932ded
glennlopez <glennlopez@gmail.com>
2016-01-02 09:12:25 -0800

    update

---
+++ zyBooks/basic_class_use.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Print person1's kids, apply the IncNumKids() function, and print again, outputting text as below. End each line with newline.
+	Sample output for below program:
+		Kids: 3
+		New baby, kids now: 4
+*/
+
+class PersonInfo {
+   public:
+      void   SetNumKids(int personsKids);
+      void   IncNumKids();
+      int    GetNumKids() const;
+   private:
+      int    numKids;
+};
+
+void PersonInfo::SetNumKids(int personsKids) {
+   numKids = personsKids;
+   return;
+}
+
+void PersonInfo::IncNumKids() {
+   numKids = numKids + 1;
+   return;
+}
+
+int PersonInfo::GetNumKids() const {
+   return numKids;
+}
+
+int main() {
+   PersonInfo person1;
+
+   person1.SetNumKids(3);
+
+   /* Your solution goes here  */
+
+   return 0;
+}
