786479eac9107d76fbaf48d8e43342449396a61e
glennlopez <glennlopez@gmail.com>
2016-01-04 22:07:31 -0800

    update

--- zyBooks/7.2 - Classes Intro/sandbox/one_practice.cpp
+++ zyBooks/7.2 - Classes Intro/sandbox/one_practice.cpp
@@ -9,24 +9,22 @@ using namespace std;
 		[x] See Account information
 		[x] Deposit money
 		[x] Withdraw money
-		[ ] Menu for manipulation
+		[x] Menu for manipulation
 
 	Bonus:
 		[ ] Allow user to add chequing, savings, or join accounts
 		[ ] Allow users to add more users accounts
 */
 
-//** ACCOUNT MANIPULATOR FUNCTIONS **//
-
 
 class BankOfGlenn{
 	public:
 		//account manipulators
-		void WithdrawAction(double modifier);	//Withdraw
+		void WithdrawAction(double modifier);		//Withdraw
 		void DepositeAction(double modifier);		//deposit
 
 		//account information
-		void PrintInfo(); 						//print info gui
+		void DisplayInfo(); 						//print info gui
 		void AccID(int param);					//account id
 		void AccBal(double param);				//account ballance
 		void SetFname(string param);			//first name
@@ -64,7 +62,7 @@ void BankOfGlenn::AccID(int param){
 
 }
 
-void BankOfGlenn::PrintInfo(){
+void BankOfGlenn::DisplayInfo(){
 	system("clear");
 	cout << "------------------------" << endl;
 	cout << "Account ID: " << accountid << endl;
@@ -73,6 +71,9 @@ void BankOfGlenn::PrintInfo(){
 	cout << "------------------------" << endl;
 }
 
+
+
+//** FUNCTIONS **//
 void AskUserName(string& fn_param, string& ln_param){
 	cout << "Banker Name (firstname lastname): ";
 	cin >> fn_param;
@@ -96,11 +97,46 @@ int main(int argc, char const *argv[]) {
 	myAccount.SetFname(fn_param);
 	myAccount.SetLname(ln_param);
 
-	myAccount.PrintInfo();
+	//display menu
+	char usrChoice = '?';
+	double modifier;
+	bool display = 1;
 
+	do {
+			myAccount.DisplayInfo();
+			cout << "Make a selection(w/d/q): ";
+			cin >> usrChoice;
 
+			switch (usrChoice) {
+				case 'w':
+				system("clear");
+				myAccount.DisplayInfo();
+				cout << "How much to withdraw: ";
+				cin >> modifier;
+				cout << endl;
+				myAccount.WithdrawAction(modifier);
+				myAccount.DisplayInfo();
+				break;
 
+				case 'd':
+				system("clear");
+				myAccount.DisplayInfo();
+				cout << "How much to deposit: ";
+				cin >> modifier;
+				cout << endl;
+				myAccount.DepositeAction(modifier);
+				myAccount.DisplayInfo();
+				break;
 
+				case 'q':
+				myAccount.DisplayInfo();
+				display = 0;
+				break;
 
+				default:
+				myAccount.DisplayInfo();
+				break;
+		}
+	}while (display);
 	return 0;
 }
---
+++ zyBooks/7.2 - Classes Intro/sandbox/vector_manipulate.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+	vector<string> usrStr(2);
+	
+
+	return 0;
+}
