cf940ea312fa7769547ff84a30feb356c2ceed24
glennlopez <glennlopez@gmail.com>
2016-01-17 04:48:10 -0800

    update

--- practice/practice_insert_sorter.cpp
+++ practice/practice_insert_sorter.cpp
@@ -8,8 +8,31 @@ using namespace std;
 /*
 	Run the program and observe the output to be: 55 4 250 19. Modify the numsInsert function to insert each item in sorted order. The new program should output: 4 19 55 250
 
-		using vecotr.insert(vector.begin() + x, x);
+		using vector.insert(vector.begin() + x, x);
 */
+
+void numsInsert(vector<int>& numsList, int numVal){
+	unsigned int i = 0;
+	for(i = 0; i < numsList.size(); i++){
+		if(numVal < numsList.at(i)){
+			numsList.insert(numsList.begin() + i, numVal);
+			break;
+		}
+	}
+
+	if(i == numsList.size()){
+		numsList.push_back(numVal);
+	}
+
+}
+
+void numsPrint(const vector<int>& numsList){
+	unsigned int i = 0;
+	for(i = 0; i < numsList.size(); i++){
+		cout << numsList.at(i) << " ";
+	}
+	cout << endl;
+}
 
 int main(){
 	// DO NOT MODIFY BELOW
