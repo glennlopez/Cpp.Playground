fefa359b9757fd0b95ea18bb53807af732d07721
glennlopez <glennlopez@gmail.com>
2016-01-17 15:05:10 -0800

    update

--- practice/vector_add_class.cpp
+++ practice/vector_add_class.cpp
@@ -12,16 +12,32 @@ public:
 		size = players.size();
 	}
 
+	void Add(int addSize){
+		size = size + addSize;
+	}
 };
+
+void Print(vector<int>& vector){
+	unsigned int i = 0;
+	for(i = 0; i < vector.size(); i++){
+		cout << vector.at(i) << endl;
+	}
+}
 
 int main(){
 	vector<int> players;
 	vector<int> teams;
 	AddMore(players, 13);
 	AddMore(players, 23);
+	AddMore(teams, 1);
+	AddMore(teams, 12);
 
-	cout << players.at(0) << endl;
-	cout << players.at(1) << endl;
+	Print(teams);
+
+	// cout << players.at(0) << endl;
+	// cout << players.at(1) << endl;
+	// cout << teams.at(0) << endl;
+	// cout << teams.at(1) << endl;
 
 
 	return 0;
