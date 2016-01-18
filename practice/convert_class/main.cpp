d7e553cc80233625b093a8d44fe28b1ac74c01b9
glennlopez <glennlopez@gmail.com>
2016-01-18 07:54:00 -0800

    update

---
+++ practice/convert_class/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "convert_class.cpp"
+using namespace std;
+
+int main(){
+	Convert temp1;
+
+	cout << temp1.celcius(750, 'k') << endl;
+
+	cout << endl;
+	return 0;
+}
