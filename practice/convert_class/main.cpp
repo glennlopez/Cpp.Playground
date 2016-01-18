afc0d69a1e4ce2fad06ea0c6d20db41bb0064384
glennlopez <glennlopez@gmail.com>
2016-01-18 08:52:57 -0800

    finished convert_class

--- practice/convert_class/main.cpp
+++ practice/convert_class/main.cpp
@@ -2,10 +2,18 @@
 #include "convert_class.cpp"
 using namespace std;
 
+//How to use Example:
 int main(){
-	Convert temp1;
 
-	cout << temp1.celcius(750, 'k') << endl;
+	Convert temp1, temp2, temp3;
+
+	//cout << temp1.celcius(750, 'k') << endl;
+	//cout << temp2.fahrenheit(-30, 'c') << endl;
+	//cout << temp3.kelvin(230, 'f') << endl;
+
+	//celcius(200, 'f');
+	kelvin(33, 'c');
+	//fahrenheit(38, 'c');
 
 	cout << endl;
 	return 0;
