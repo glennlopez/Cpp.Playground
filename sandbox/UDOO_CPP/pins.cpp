380561911fdce41cbbd9369e1363d965dd95f541
glennlopez <glennlopez@gmail.com>
2016-01-27 14:57:31 -0800

    update

---
+++ sandbox/UDOO_CPP/pins.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include "pins.h"
+using namespace std;
+
+void pin(int pinNumber, int pinValue){
+
+}
+
+void pin(int pinNumber, char pinDirection){
+
+}
