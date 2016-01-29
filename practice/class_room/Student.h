2a761a53187242335a836f95b7c7d4c3752aadb0
glennlopez <glennlopez@gmail.com>
2016-01-28 19:56:38 -0800

    added public class initiators

--- practice/class_room/Student.h
+++ practice/class_room/Student.h
@@ -10,11 +10,22 @@ class StudentInfo{
 private:
 	string name_first;
 	string name_last;
+
 	unsigned int birth_y;
 	unsigned int birth_m;
 	unsigned int birth_d;
 
+	char gender;
+
 public:
+	//default constructor
+	StudentInfo();
+
+	//overload constructor
+
+	//accessors
+
+	//mutators
 
 
 };
