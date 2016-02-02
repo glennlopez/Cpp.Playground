8db00a156c611dbcb6e2e209ca368e1319a44f35
glennlopez <glennlopez@gmail.com>
2016-02-01 20:33:04 -0800

    started lesson

--- practice/heap_memory.cpp
+++ practice/heap_memory.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
-using namespace std;
+//using namespace std;
+using std::cout;
+using std::endl;	//<-- to save codespace we stop using namespace 
+
+/*
+	A look at dynamically alocated memory
+	(pointers and heap memory)
+*/
 
 int main(){
+
+	int* p = new int;
+
+	new int;
 
 	cout << endl;
 	return 0;
