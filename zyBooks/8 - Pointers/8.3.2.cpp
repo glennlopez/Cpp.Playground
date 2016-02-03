dd341678ba2901af277033941e3975c093f31507
glennlopez <glennlopez@gmail.com>
2016-02-02 21:53:29 -0800

    initial commit

---
+++ zyBooks/8 - Pointers/8.3.2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Deallocate memory for kitchenPaint using the delete operator.
+*/
+
+class PaintContainer {
+   public:
+      ~PaintContainer();
+      double gallonPaint;
+};
+
+PaintContainer::~PaintContainer() { // Covered in section on Destructors.
+   cout << "PaintContainer deallocated." << endl;
+   return;
+}
+
+int main() {
+   PaintContainer* kitchenPaint;
+
+   kitchenPaint = new PaintContainer;
+   kitchenPaint->gallonPaint = 26.3;
+
+   /* Your solution goes here  */
+
+   return 0;
+}
