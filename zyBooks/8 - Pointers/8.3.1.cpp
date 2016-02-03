daf708b64956d02a152a70a31fc2edb93a45290b
glennlopez <glennlopez@gmail.com>
2016-02-02 21:59:43 -0800

    solved

--- zyBooks/8 - Pointers/8.3.1.cpp
+++ zyBooks/8 - Pointers/8.3.1.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 /*
-	Allocate memory for houseHeight using the new operator. 
+	Allocate memory for houseHeight using the new operator.
 */
 
 int main() {
    double* houseHeight = 0;
 
    /* Your solution goes here  */
+	houseHeight = new double;
 
    *houseHeight = 23;
    cout << "houseHeight is " << *houseHeight;
