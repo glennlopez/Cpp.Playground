a02a8fb440919de30ddc220daf19b9d168d31276
glennlopez <glennlopez@gmail.com>
2016-02-06 17:19:37 -0800

    update

---
+++ zyBooks/8 - Pointers/8.4.1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+/*
+	Assign a pointer to any instance of searchChar in personName to searchResult.
+*/
+
+int main() {
+   char personName[100] = "Albert Johnson";
+   char searchChar = 'J';
+   char* searchResult = 0;
+
+   /* Your solution goes here  */
+
+   if (searchResult != 0) {
+      cout << "Character found." << endl;
+   }
+   else {
+      cout << "Character not found." << endl;
+   }
+
+   return 0;
+}
---
+++ zyBooks/8 - Pointers/8.4.2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+/*
+	Assign the first instance of The in movieTitle to movieResult. 
+*/
+
+int main() {
+   char movieTitle[100] = "The Lion King";
+   char* movieResult = 0;
+
+   /* Your solution goes here  */
+
+   cout << "Movie title contains The? ";
+   if (movieResult != 0) {
+      cout << "Yes." << endl;
+   }
+   else {
+      cout << "No." << endl;
+   }
+
+   return 0;
+}
