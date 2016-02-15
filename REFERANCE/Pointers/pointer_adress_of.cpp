89b639241f5ef7d197077feab013b1ef71cf56c4
glennlopez <glennlopez@gmail.com>
2016-02-15 06:59:59 -0800

    initial commit

---
+++ REFERANCE/Pointers/pointer_adress_of.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int a;
+	int b;
+
+	//& means "address of" so cout << &a will output the address of int a
+	cout << &a;
+
+	cout << endl;
+	return 0;
+}
