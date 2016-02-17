2d3d985ee6212acec55ef6ab19a1b24067b94a87
glennlopez <glennlopez@gmail.com>
2016-02-17 01:39:38 -0800

    initial commit

---
+++ tools/primeNum_calc_function/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "calcPrime.cpp"
+using namespace std;
+
+/* This is a placeholder for testing out the functions of
+ *	clacPrime.cpp. It's a simple 6n-1 6n+1 calculator for
+ * determining if a number (small or large) is a prime number
+ */
+
+int main(){
+
+	cout << endl;
+	return 0;
+}
