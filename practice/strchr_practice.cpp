1cd8fe0b91681eda4cba95dd413e9485e817e350
glennlopez <glennlopez@gmail.com>
2016-02-20 15:02:30 -0800

    update

---
+++ practice/strchr_practice.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+	const short MAX_CHAR = 100;
+	char userInput[MAX_CHAR];
+	char *firstR;
+
+	//get input from user
+	cout << "Say something: ";
+	cin.getline(userInput, MAX_CHAR);
+
+
+
+
+	cout << endl;
+	return 0;
+}
