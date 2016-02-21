42a74f5fab69df174decb6dfdff40d1c1d68beaf
glennlopez <glennlopez@gmail.com>
2016-02-21 12:41:42 -0800

    added comment: how my lcm algorythm works

--- tools/gcfCalc_euclid/calcLCM.cpp
+++ tools/gcfCalc_euclid/calcLCM.cpp
@@ -1,8 +1,17 @@
-//#include "calcGCF.cpp"
+//#include "calcGCF.cpp"		//<-- This requires GCF.cpp
 
 int lcm(int x, int y){
 	int gcf_div;
 	gcf_div = gcf(x, y);
+
+	/* The formula for finding the LCM is:
+	 *		(num1 * num2)/GCF
+	 *
+	 * To find GCF:
+	 *		Replace the larger number with the diffrance between the two
+	 *		numbers until both numbers are equal.
+	 *		(irl: factor tree will also work but slower)
+	 */
 
 	return ((x * y)/gcf_div);
 }
