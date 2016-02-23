0ee4a2f449273b1948a7c550ca569b905bc3cfaa
glennlopez <glennlopez@gmail.com>
2016-02-23 09:57:24 -0800

    added listPrimes.cpp for listing all primes

--- tools/gcfCalc_euclid/listPrimes.cpp
+++ tools/gcfCalc_euclid/listPrimes.cpp
@@ -1,1 +1,27 @@
 //This will list x number of prime numbers as defined by user
+#include <iostream>
+#include <vector>
+//#include "calcPrime.cpp"					//<-- required for telling if a num is prime
+using namespace std;
+
+//list x prime numbers
+void listPrimes(int num){
+	vector<int> primes(0);
+
+	int storedPrimes = 0;
+	int i;
+
+	//store prime numbers in a vactor
+	while(num != storedPrimes){			//<-- loops until conditions are met
+		i++;
+		if(calcPrime(i) == 1){
+			primes.push_back(i);
+			storedPrimes++;
+		}
+	}
+
+	//print vector elements
+	for(unsigned int i = 0; i < primes.size(); i++){
+		cout << primes.at(i) << " ";
+	}
+}
