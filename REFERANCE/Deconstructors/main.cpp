da790f7e67d6cd69af960f680a834dbc8a0334b5
glennlopez <glennlopez@gmail.com>
2016-02-24 12:24:07 -0800

    added comments

--- REFERANCE/Deconstructors/main.cpp
+++ REFERANCE/Deconstructors/main.cpp
@@ -3,11 +3,15 @@
 #include "Students.cpp"
 using namespace std;
 
-/* Youtube link:
- *		https://www.youtube.com/watch?v=4P4Im0vF_mU
- */
+/*
+	NOTE: A destructor is typically needed whenever a constructor acquires resources. One common example of acquiring resources is when the constructor allocates memory, as above, in which case the destructor might free that memory. Another example is when a constructor opens a file, in which case the destructor might close that file.
+
+		https://www.youtube.com/watch?v=4P4Im0vF_mU
+*/
+
 
 int main(){
+
 	Students *glenn;
 	glenn = new Students;
 	delete glenn;
