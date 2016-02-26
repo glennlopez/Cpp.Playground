4f9e88df93e5c7385c3aa81d31827567378b69fe
glennlopez <glennlopez@gmail.com>
2016-02-26 14:25:25 -0800

    update

--- REFERANCE/Copy_Constructor/main.cpp
+++ REFERANCE/Copy_Constructor/main.cpp
@@ -3,11 +3,16 @@
 #include "Human.cpp"
 using namespace std;
 
+//System includes for terminal system() calls
+#include <stdio.h>
+#include <stdlib.h>
+
 /*
 	https://www.youtube.com/watch?v=2KvslbLHTUY
 */
 
 int main(){
+	system("clear");
 
 	Human Glenn("Glenn", 12);
 	Human Clone = Glenn;
