3b3a4806f468dc72919e07346edcfe0d8947b9e6
glennlopez <glennlopez@gmail.com>
2016-03-04 22:54:50 -0800

    init comit

---
+++ zyBooks/9 - Streams/9.3.1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <ios>
+#include <iomanip>
+using namespace std;
+
+/*
+	Write a single statement that prints outsideTemperature with 4 digits. End with newline. Sample output:
+					103.5
+*/
+
+int main() {
+   double outsideTemperature = 103.45632;
+
+   /* Your solution goes here  */
+
+   return 0;
+}
