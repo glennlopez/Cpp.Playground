25bc28b11452b16cbc7214b0d30c0ebdb5ba47f4
glennlopez <glennlopez@gmail.com>
2016-03-04 20:58:35 -0800

    initial commit

---
+++ REFERANCE/output_formatting.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+int main(){
+	float num1 = 112.12234;
+	float num2 = 21;
+
+	cout << setprecision(2) << num1 << endl;
+	cout << setprecision(3) << num1 << endl;
+	cout << setprecision(4) << num1 << endl;
+	cout << setprecision(5) << num1 << endl;
+	cout << fixed << num1 << endl;
+	cout << scientific << num1 << endl;
+	cout << scientific << setprecision(2) << num1 << endl;
+
+	cout << fixed << showpoint << num2 << endl;
+
+
+
+	cout << endl;
+	return 0;
+}
