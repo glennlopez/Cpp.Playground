12cae5d9dd49ada126b3c4f25f7b4b13db56d5df
glennlopez <glennlopez@gmail.com>
2016-03-26 01:37:31 -0700

    nightly update

---
+++ tools/keyGenerator/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "FindFactors.cpp"
+#include "key_check.cpp"
+//#include "listPrime.cpp"
+using namespace std;
+
+int main(){
+
+	//calc_primeFactor(127157243331463);
+	//listPrimes(900000);
+	key_check(22552856);
+
+
+	return 0;
+}
