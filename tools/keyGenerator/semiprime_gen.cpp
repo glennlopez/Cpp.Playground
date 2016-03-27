47cfcba65ee7aa6060a1617d01c48dab9215c5a4
glennlopez <glennlopez@gmail.com>
2016-03-27 13:42:11 -0700

    added random number generator

--- tools/keyGenerator/semiprime_gen.cpp
+++ tools/keyGenerator/semiprime_gen.cpp
@@ -1,5 +1,11 @@
 ull semiprime_gen(vector<ull>& primes){
+	//seed a random number generator
+
+	//variables for storage
 	ull semiprime;
+
+	//generate primes
+
 
 	semiprime = primes.at(0) * primes.at(1);
 
