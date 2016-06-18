f4f8e63becc6d791ae156740ab08971860d717b7
glennlopez <glennlopez@gmail.com>
2016-06-18 14:46:39 -0700

    update

--- tools/keyGenerator/semiprime_gen.cpp
+++ tools/keyGenerator/semiprime_gen.cpp
@@ -1,11 +1,15 @@
-ull semiprime_gen(vector<ull>& primes){
-	//seed a random number generator
+#include "rNum_gen.cpp"
+
+ull semiprime_gen(vector<ull>& primes, unsigned int range, ull startingPoint){
+
+	//random number generator
+	ull q = rNum_gen(range, (startingPoint * rNum_gen(300, 700)/9));
+	ull k = rNum_gen(range, (startingPoint * rNum_gen(500, 200)/8));
 
 	//variables for storage
 	ull semiprime;
-
-	//generate primes
-
+	prime_gen(primes, 1, q);	//stored in vector outside of function
+	prime_gen(primes, 1, k); 	//stored in vector outside of function
 
 	semiprime = primes.at(0) * primes.at(1);
 
