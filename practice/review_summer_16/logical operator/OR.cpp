45ac19697bad69d0f5e221ccef9c7e024d859d99
glennlopez <glennlopez@gmail.com>
2016-06-18 20:39:23 -0700

    restructured review folder

---
+++ practice/review_summer_16/logical operator/OR.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+//logical or operation using ||
+//https://zybooks.zyante.com/#/zybook/ProgrammingInCppR11/chapter/3/section/4
+
+int main(){
+
+	return 0;
+}
