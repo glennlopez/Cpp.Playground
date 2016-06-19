89783511580c02364b4aa26a433264f3697372bf
glennlopez <glennlopez@gmail.com>
2016-06-19 12:31:40 -0700

    update

--- practice/review_summer_16/logical operator/OR.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-//logical or operation using ||
-//https://zybooks.zyante.com/#/zybook/ProgrammingInCppR11/chapter/3/section/4
-
-int main(){
-
-	return 0;
-}
