ad6ffcd27ab838d8bdd00945ffe2cfc932401eca
glennlopez <glennlopez@gmail.com>
2016-06-19 16:11:42 -0700

    update

---
+++ zyBooks/4 - Loops/4.4 - Counting/4.4.1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+//https://zybooks.zyante.com/#/zybook/ProgrammingInCppR11/chapter/4/section/4
+
+int main() {
+   int userNum = 0;
+   int i = 0;
+
+   userNum = 4;    // Assume positive
+
+   /* Your solution goes here  */
+
+   cout << endl;
+
+   return 0;
+}
