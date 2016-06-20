4073ce020d5cfa891781d90b724d88ab41aea63a
glennlopez <glennlopez@gmail.com>
2016-06-19 20:26:37 -0700

    added 2.4.2

---
+++ zyBooks/2 - Variables and assignments/2.4 - Arith expression (int)/2.4.2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Write a statement that computes num1 plus num2, divides by 3, and assigns the result to finalResult. Ex: If num1 is 4 and num2 is 5, final result is 3. 
+*/
+
+int main() {
+   int num1 = 0;
+   int num2 = 0;
+   int finalResult = 0;
+
+   num1 = 4;
+   num2 = 5;
+
+   /* Your solution goes here  */
+
+   cout << "Final result: " << finalResult << endl;
+
+   return 0;
+}
