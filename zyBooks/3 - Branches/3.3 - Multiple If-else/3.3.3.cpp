4a94649b83a9522b26a815e503c6ff39bac67d92
glennlopez <glennlopez@gmail.com>
2016-06-20 20:50:33 -0700

    FIX ME!

--- zyBooks/3 - Branches/3.3 - Multiple If-else/3.3.3.cpp
+++ zyBooks/3 - Branches/3.3 - Multiple If-else/3.3.3.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 
 /*
-
 	Write an if-else statement with multiple branches. If givenYear is 2100 or greater, print "Distant future" (without quotes). Else, if givenYear is 2000 or greater (2000-2099), print "21st century". Else, if givenYear is 1900 or greater (1900-1999), print "20th century". Else (1899 or earlier), print "Long ago". Do NOT end with newline.
-
 */
 
 int main() {
@@ -13,6 +11,17 @@ int main() {
    givenYear = 1776;
 
    /* Your solution goes here  */
+	if(givenYear >= 2100){
+		cout << "Distant future" << endl;
+	}
+
+	else if((givenYear >= 2000) || (givenYear  <= 2099)){
+		cout << "21st century" << endl;
+	}
+
+	else{
+		cout << "Long ago" << endl;
+	}
 
    return 0;
 }
