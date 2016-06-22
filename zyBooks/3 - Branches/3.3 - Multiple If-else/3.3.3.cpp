4d712293e878a5555da09bb429db39e79eefcac1
glennlopez <glennlopez@gmail.com>
2016-06-21 18:18:30 -0700

    re-made 3.3.3

--- zyBooks/3 - Branches/3.3 - Multiple If-else/3.3.3.cpp
+++ zyBooks/3 - Branches/3.3 - Multiple If-else/3.3.3.cpp
@@ -11,16 +11,21 @@ int main() {
    givenYear = 1776;
 
    /* Your solution goes here  */
+
+	//If givenYear is 2100 or greater, print "Distant future"
 	if(givenYear >= 2100){
 		cout << "Distant future" << endl;
 	}
 
-	else if((givenYear >= 2000) || (givenYear  <= 2099)){
-		cout << "21st century" << endl;
+	//Else, if givenYear is 2000 or greater (2000-2099), print "21st century"
+
+	else if (givenYear < 2100 && givenYear > 1999){
+		cout << "20th century" << endl;
 	}
 
+	//Else, if givenYear is 1900 or greater (1900-1999), print "20th century". Else (1899 or earlier), print "Long ago". Do NOT end with newline.
 	else{
-		cout << "Long ago" << endl;
+		cout << "Long ago";
 	}
 
    return 0;
