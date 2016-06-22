97283f0f55e72e02c86e8d13c4b6e49f86e556ba
glennlopez <glennlopez@gmail.com>
2016-06-21 19:14:25 -0700

    solved 3.3.4

--- zyBooks/3 - Branches/3.3 - Multiple If-else/3.3.4.cpp
+++ zyBooks/3 - Branches/3.3 - Multiple If-else/3.3.4.cpp
@@ -4,7 +4,7 @@ using namespace std;
 /*
 
 	Write multiple if statements. If carYear is 1969 or earlier, print "Probably has few safety features." If 1970 or higher, print "Probably has seat belts." If 1990 or higher, print "Probably has anti-lock brakes." If 2000 or higher, print "Probably has air bags." End each phrase with period and newline. Ex: carYear = 1995 prints:
-	
+
 		Probably has seat belts.
 		Probably has anti-lock brakes.
 
@@ -16,6 +16,25 @@ int main() {
    carYear = 1940;
 
    /* Your solution goes here  */
+	if(carYear <= 1969){
+		cout << "Probably has a few safety features";
+		cout << endl;
+	}
+
+	if(carYear >= 1970){
+		cout << "Probably has seat belts";
+		cout << endl;
+	}
+
+	if(carYear >= 1990){
+		cout << "Probobly has anti-lock breaks";
+		cout << endl;
+	}
+
+	if(carYear >= 2000){
+		cout << "Probably has airbags";
+		cout << endl;
+	}
 
    return 0;
 }
