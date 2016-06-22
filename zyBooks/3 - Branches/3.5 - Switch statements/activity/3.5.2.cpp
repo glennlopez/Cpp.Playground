89f9605e5128785f177330078eb9b087b4d638c9
glennlopez <glennlopez@gmail.com>
2016-06-21 19:48:03 -0700

    organized 3.5 folder

---
+++ zyBooks/3 - Branches/3.5 - Switch statements/activity/3.5.2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/*
+Write a switch statement that checks origLetter. If 'a' or 'A', print "Alpha". If 'b' or 'B', print "Beta". For any other character, print "Unknown". Use fall-through as appropriate. End with newline.
+*/
+
+int main() {
+   char origLetter = '?';
+
+   origLetter = 'a';
+
+   /* Your solution goes here  */
+
+
+   return 0;
+}
