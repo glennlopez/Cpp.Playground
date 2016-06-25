c6bba35a93700f84626373e49be3bc7d35c6cda9
glennlopez <glennlopez@gmail.com>
2016-06-25 15:41:56 -0700

    added missing lessons

---
+++ zyBooks/2 - Variables and assignments/2.8 - Type conversions/activities/int_double.cpp
@@ -0,0 +1,15 @@
+//https://zybooks.zyante.com/#/zybook/ProgrammingInCppR11/chapter/2/section/8
+#include <iostream>
+using namespace std;
+
+int main(){
+	double num1 = 2; 	//change to double or int to see what happens
+	int num2 = 5;		//change to double or int to see what happens
+	double total;		//change to double or int to see what happens 
+
+	total = num1 / num2;
+	cout << total;
+
+	cout << endl;
+	return 0;
+}
