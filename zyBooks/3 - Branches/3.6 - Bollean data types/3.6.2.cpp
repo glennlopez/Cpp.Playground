1bd5d852894e456f1902dcef518bbd64829573c7
glennlopez <glennlopez@gmail.com>
2016-06-25 18:43:24 -0700

    added 3.6.2

---
+++ zyBooks/3 - Branches/3.6 - Bollean data types/3.6.2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Write an if-else statement to describe an object. Print "Balloon" if isBalloon is true and isRed is false. Print "Red balloon" if isBalloon and isRed are both true. Print "Not a balloon" otherwise. End with newline.
+*/
+
+int main() {
+   bool isRed = false;
+   bool isBalloon = false;
+
+   /* Your solution goes here  */
+
+   return 0;
+}
