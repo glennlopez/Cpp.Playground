7d295ee1ea540fd8e917cfe7df3be82774b48e9f
glennlopez <glennlopez@gmail.com>
2016-06-25 17:06:47 -0700

    solved 3.5.1

--- zyBooks/3 - Branches/3.5 - Switch statements/3.5.1.cpp
+++ zyBooks/3 - Branches/3.5 - Switch statements/3.5.1.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 /*
-	Write a switch statement that checks nextChoice. If 0, print "Rock". If 1, print "Paper". If 2, print "Scissors". For any other value, print "Unknown". End with newline. Do not get input from the user; nextChoice is assigned in main(). 
+	Write a switch statement that checks nextChoice. If 0, print "Rock". If 1, print "Paper". If 2, print "Scissors". For any other value, print "Unknown". End with newline. Do not get input from the user; nextChoice is assigned in main().
 */
 
 int main() {
@@ -11,6 +11,24 @@ int main() {
    nextChoice = 2;
 
    /* Your solution goes here  */
+	switch (nextChoice) {
+		case 0:
+			cout << "Rock";
+		break;
 
+		case 1:
+			cout << "Paper";
+		break;
+
+		case 2:
+			cout << "Scissors";
+		break;
+
+		default:
+			cout << "Unknown";
+		break;
+	}
+
+	cout << endl;
    return 0;
 }
