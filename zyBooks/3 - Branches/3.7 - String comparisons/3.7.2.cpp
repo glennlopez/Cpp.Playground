46a5460efe6f359331da2e761ee4059347ae2e48
glennlopez <glennlopez@gmail.com>
2016-06-25 20:45:58 -0700

    added 3.7.2

---
+++ zyBooks/3 - Branches/3.7 - String comparisons/3.7.2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+Print the two strings in alphabetical order. Assume the strings are lowercase. End with newline. Sample output:
+
+	capes rabbits
+	
+*/
+
+int main() {
+   string firstString;
+   string secondString;
+
+   firstString = "rabbits";
+   secondString = "capes";
+
+   /* Your solution goes here  */
+
+   return 0;
+}
