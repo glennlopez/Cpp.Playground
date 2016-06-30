291ed927d63a5373f50add435a8e14e6367d686e
glennlopez <glennlopez@gmail.com>
2016-06-30 14:54:48 -0700

    added 3.8.1

---
+++ zyBooks/3 - Branches/3.8 - String access operations/3.8.1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+	Assign the size of userInput to stringSize. Ex: if userInput = "Hello", output is:
+	Size of userInput: 5
+*/
+
+int main() {
+   string userInput;
+   int stringSize = 0;
+
+   userInput = "Hello";
+
+   /* Your solution goes here  */
+
+   cout << "Size of userInput: " << stringSize << endl;
+
+   return 0;
+}
