8c643301cd8259e9fe83ac54239baf4754d961f9
glennlopez <glennlopez@gmail.com>
2016-06-30 15:12:03 -0700

    update

---
+++ zyBooks/3 - Branches/3.8 - String access operations/activity/string_length.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+//testing length() string access
+
+using namespace std;
+
+int main(){
+
+	string myString_1 = "string cheese";
+	string myString_2 = "protractor";
+
+	cout << endl;
+	return 0;
+}
