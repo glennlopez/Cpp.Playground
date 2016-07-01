8e388368c206d53f82d4125750ac1bd3b41cdc21
glennlopez <glennlopez@gmail.com>
2016-06-30 22:14:17 -0700

    added psuedo code

--- practice/string_findCharCount.cpp
+++ practice/string_findCharCount.cpp
@@ -7,6 +7,11 @@ using namespace std;
 
 int main(){
 
+	//pre define a string
+
+	//count how many times a letter occurs in the string
+		//for loop
+			//add a counter that keeps track of how many times a letter shows up
 
 	cout << endl;
 	return 0;
