92933e8ea30c2a75991695611cd4dd2142ab75fc
glennlopez <glennlopez@gmail.com>
2016-06-30 20:49:05 -0700

    added find search functions

--- zyBooks/3 - Branches/3.8 - String access operations/activity/string_findCharCount.cpp
+++ zyBooks/3 - Branches/3.8 - String access operations/activity/string_findCharCount.cpp
@@ -7,6 +7,20 @@ using namespace std;
 
 int main(){
 
+	string myString = "say cheese";
+	char findChar = '?';
+	int indexHolder = 0;
+
+	cout << "myString: " << myString << endl;
+
+	cout << "Type in a letter to count: ";
+	cin >> findChar;
+
+	if(myString.find(findChar) > 0){
+		cout << "testing";
+	}
+
+
 
 	cout << endl;
 	return 0;
