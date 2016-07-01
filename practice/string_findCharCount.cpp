5641bce89c4723975181798a8cb3d0efd792339c
glennlopez <glennlopez@gmail.com>
2016-06-30 22:11:25 -0700

    redux

--- practice/string_findCharCount.cpp
+++ practice/string_findCharCount.cpp
@@ -7,28 +7,6 @@ using namespace std;
 
 int main(){
 
-	string myString = "say cheese";
-	char findChar = '?';
-	int indexHolder = 0;
-	int maxIndex = myString.length();
-
-	cout << "myString = " << myString << endl;
-
-	cout << "Type in a letter to count: ";
-	cin >> findChar;
-	cout << "---------------------------" << endl;
-
-	if( (myString.find(findChar) >= 0) && (myString.find(findChar) < maxIndex) ){
-		indexHolder = indexHolder + myString.find(findChar);
-	}
-
-	//-- start debug code
-	cout << "Location: " << myString.find(findChar) << endl;
-	cout << "String Length: " << maxIndex << endl;
-	cout << "New Index: " << indexHolder << endl;
-	//-- end of debug
-
-
 
 	cout << endl;
 	return 0;
