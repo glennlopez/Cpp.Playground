92007ab75b8f2585f1b86721a3145b7bd29bcfa2
glennlopez <glennlopez@gmail.com>
2016-06-30 20:56:22 -0700

    added indexing limit

--- zyBooks/3 - Branches/3.8 - String access operations/activity/string_findCharCount.cpp
+++ zyBooks/3 - Branches/3.8 - String access operations/activity/string_findCharCount.cpp
@@ -9,16 +9,23 @@ int main(){
 
 	string myString = "say cheese";
 	char findChar = '?';
-	int indexHolder = 0;
+	int indexHolder = -1;
+	int maxIndex = myString.length();
 
 	cout << "myString: " << myString << endl;
 
 	cout << "Type in a letter to count: ";
 	cin >> findChar;
 
-	if(myString.find(findChar) > 0){
-		cout << "testing";
+	if( (myString.find(findChar) >= 0) && (myString.find(findChar) < maxIndex) ){
+		cout << "//branch triggered" << endl;
 	}
+
+	//-- start debug code
+	cout << myString.find(findChar);
+	cout << endl;
+	cout << maxIndex;
+	//-- end of debug
 
 
 
