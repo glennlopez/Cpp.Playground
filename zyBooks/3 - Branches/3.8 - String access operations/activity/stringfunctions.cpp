99bcfb85e44e2d65ce23eb551841d1269a52fa54
glennlopez <glennlopez@gmail.com>
2016-06-30 18:39:04 -0700

    added 4 possible string function

--- zyBooks/3 - Branches/3.8 - String access operations/activity/stringfunctions.cpp
+++ zyBooks/3 - Branches/3.8 - String access operations/activity/stringfunctions.cpp
@@ -9,14 +9,20 @@ int main(){
 
 	string myString_1 = "string cheese";
 	string myString_2 = "protractor";
+	string emptyString = "";
 
 	int strLen1 = myString_1.length();
 	int strLen2 = myString_2.length();
 	int strFind = myString_1.find('e');
+	bool isEmpty = emptyString.empty();
+
 
 	cout << "STRLen1: " << strLen1 << endl;
 	cout << "STRLen2: " << strLen2 << endl;
 	cout << "STRFind: " << strFind << endl;
+	cout << "STREmpty: " << isEmpty << endl;
+
+	cout << "StringSub: " << myString_2.substr(myString_2.length() - 7, 7);
 
 	cout << endl;
 	return 0;
