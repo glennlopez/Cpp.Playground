abad2317d5157e3e44dbf51b008c37848fdce584
glennlopez <glennlopez@gmail.com>
2016-07-01 11:46:50 -0700

    initial commit

---
+++ zyBooks/3 - Branches/3.8 - String access operations/3.8.3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+	Print "Censored" if userInput contains the word "darn", else print userInput. End with newline.
+
+	Note: These activities may test code with different test values. This activity will perform three tests, with userInput of "That darn cat.", then with "Dang, that was scary!", then with "I'm darning your socks.". See How to Use zyBooks.
+
+	Also note: If the submitted code has an out-of-range access., the system will stop running the code after a few seconds, and report "Program end never reached." The system doesn't print the test case that caused the reported message.
+*/
+
+int main() {
+   string userInput;
+
+   userInput = "That darn cat.";
+
+   /* Your solution goes here  */
+
+   return 0;
+}
