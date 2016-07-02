101216b41dcbb9f929caf2d4327ca91b399dce44
glennlopez <glennlopez@gmail.com>
2016-07-02 15:31:01 -0700

    completed push_back example

--- zyBooks/3 - Branches/3.9 - String modify operations/push_back/simple_pushback.cpp
+++ zyBooks/3 - Branches/3.9 - String modify operations/push_back/simple_pushback.cpp
@@ -4,9 +4,20 @@
 using namespace std;
 
 string myString = "?";
+string countString = "";
+int i = 0;
 
 int main(){
 
+	myString.push_back('!');
+
+	for(i = 0; i < 10; i++){
+		//countString.push_back(1); //<-- not gonna work, need to use vectors
+		//countString.push_back(' ');
+	}
+	cout << i << endl;
+	cout << myString << endl;
+	cout << countString << endl;
 
 	cout << endl;
 	return 0;
