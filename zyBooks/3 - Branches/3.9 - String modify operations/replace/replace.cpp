aa1e0ad0648cfb2fe04204e6cd96f16b548751b3
glennlopez <glennlopez@gmail.com>
2016-07-02 18:13:00 -0700

    update

---
+++ zyBooks/3 - Branches/3.9 - String modify operations/replace/replace.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//https://zybooks.zyante.com/#/zybook/ProgrammingInCppR11/chapter/3/section/9
+//replace(indx, num, subStr)
+
+/*
+	Replaces characters at indices indx
+	to indx+num with a copy of subStr.
+*/
+
+int main(){
+
+	cout << endl;
+	return 0;
+}
