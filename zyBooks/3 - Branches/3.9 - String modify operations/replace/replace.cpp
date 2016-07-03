523c7701398b83ae69f971966d505b3d7ed55b42
glennlopez <glennlopez@gmail.com>
2016-07-03 15:28:01 -0700

    finished replace example

--- zyBooks/3 - Branches/3.9 - String modify operations/replace/replace.cpp
+++ zyBooks/3 - Branches/3.9 - String modify operations/replace/replace.cpp
@@ -3,14 +3,28 @@
 using namespace std;
 
 //https://zybooks.zyante.com/#/zybook/ProgrammingInCppR11/chapter/3/section/9
-//replace(indx, num, subStr)
 
 /*
 	Replaces characters at indices indx
 	to indx+num with a copy of subStr.
+	replace(indx, num, subStr)
+
+	userText is "You have many gifts"
+	userText.replace(9, 4, "a plethora of");
+	Now "You have a plethora of gifts"
 */
 
 int main(){
+
+	string charName;
+	string myString = "Hi, my name is roxan, how are you doing?";
+
+	cout << "Enter your name: ";
+	getline(cin, charName);
+
+	myString.replace(15, 5, charName);
+
+	cout << myString << endl;
 
 	cout << endl;
 	return 0;
