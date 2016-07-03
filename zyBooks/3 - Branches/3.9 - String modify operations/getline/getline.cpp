a734eb6c8d07650b4ad38e19e72a2e1a6f750fbf
glennlopez <glennlopez@gmail.com>
2016-07-02 18:12:51 -0700

    added getline excersize

---
+++ zyBooks/3 - Branches/3.9 - String modify operations/getline/getline.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//https://www.youtube.com/watch?v=uB7g9hc8WZ4
+//getline(input stream, string var, deliminator char);
+
+int main(){
+
+	cout << "What is your name: ";
+
+	cout << endl;
+	return 0;
+}
