4612db576e92c7c7ad32e379c19dacd01e818241
glennlopez <glennlopez@gmail.com>
2016-07-06 20:40:02 -0700

    initial commit

---
+++ zyBooks/3 - Branches/3.10 - Character operations/3.10.2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+/*
+	Replace any space ' ' by '_' in 2-character string passCode. Sample output for the given program:
+	1_
+*/
+
+
+int main() {
+   string passCode;
+
+   passCode = "1 ";
+
+   /* Your solution goes here  */
+
+   cout << passCode << endl;
+   return 0;
+}
