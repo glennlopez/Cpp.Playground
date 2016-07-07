d5403a7d3a5cd2d64e1dce5e8493ec7aeed65326
glennlopez <glennlopez@gmail.com>
2016-07-06 20:29:25 -0700

    initial commit

---
+++ zyBooks/3 - Branches/3.9 - String modify operations/3.9.1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+	Retype and correct the code provided to combine two strings separated by a space. Hint: What type of parameter does push_back expect?
+	   secretID.push_back(spaceChar);
+	   secretID.push_back(lastName);
+*/
+
+int main() {
+   string secretID = "Barry";
+   string lastName = "Allen";
+   char spaceChar = ' ';
+
+   /* Your solution goes here  */
+
+   cout << secretID << endl;
+   return 0;
+}
