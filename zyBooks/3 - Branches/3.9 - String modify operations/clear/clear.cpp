938378de32e2e1f0afffed4646359d604b8f1311
glennlopez <glennlopez@gmail.com>
2016-07-06 20:02:52 -0700

    fixed to show what clear() actually does

--- zyBooks/3 - Branches/3.9 - String modify operations/clear/clear.cpp
+++ zyBooks/3 - Branches/3.9 - String modify operations/clear/clear.cpp
@@ -4,20 +4,33 @@ using namespace std;
 
 //https://zybooks.zyante.com/#/zybook/ProgrammingInCppR11/chapter/3/section/9
 
+/*
+	Deletes characters, sets size to 0.
+*/
 
 
 int main(){
 
 	string myString = "my name is smith";
-	int strlegth = myString.length();
 
 	cout << endl;
-	cout << "myString: (" << myString << ") has " << strlegth << " chars." << endl;
+	cout << "myString: (" << myString << ") has " << myString.length() << " chars." << endl;
 	myString.clear();
 
 	cout << endl;
 	cout << "After myString.clear()..." << endl;
-	cout << "myString: (" << myString << ") has " << strlegth << " chars." << endl;
+	cout << "myString: (" << myString << ") has " << myString.length() << " chars." << endl;
+
+	myString = "this is his face go smush lol";
+	cout << endl;
+
+	cout << "myString (" << myString << ") not has " << myString.length() << endl;
+	myString.clear();
+	cout << endl;
+
+	cout << "myString after .clear() has " << myString.length() << endl;
+
+
 
 	cout << endl;
 	return 0;
