3ef1612227c9ba7b78581e208145c1b6699d9e80
glennlopez <glennlopez@gmail.com>
2016-07-07 17:04:12 -0700

    initial commit

---
+++ zyBooks/3 - Branches/3.10 - Character operations/isalpha/isalpha.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+using namespace std;
+
+/*
+	true if alphabetic:
+	a-z or A-Z
+
+	isalpha('x') // true
+	isalpha('6') // false
+	isalpha('!') // false
+*/
+
+int main(){
+
+
+
+	cout << endl;
+	return 0;
+}
