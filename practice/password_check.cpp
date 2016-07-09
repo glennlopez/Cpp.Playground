c346eaf20eda38271c8c5dbbfd9992c034f2e41e
glennlopez <glennlopez@gmail.com>
2016-07-08 17:28:13 -0700

    initial commit

---
+++ practice/password_check.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cctype>
+#include <string>
+using namespace std;
+
+/*
+	true if alphabetic:
+	a-z or A-Z
+
+	isalpha('x') // true
+	isalpha('6') // false
+	isalpha('!') // false
+*/
+
+int main(){
+
+	string mystring = "?";
+
+	//prompt user for passcode (cannot contain letters, min 7 numbers long)
+
+	//store passcode in myString
+
+	//check passcode length
+
+	//check passcode for letters
+
+	//prompt user to re-type passcode
+
+	//express errors to users if any
+
+	//show password stored if valid
+
+
+
+	cout << endl;
+	return 0;
+}
