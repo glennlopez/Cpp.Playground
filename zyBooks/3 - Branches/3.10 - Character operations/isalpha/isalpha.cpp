7ec6c9b7f3f33114df6b6cb0b0c1e1294fb44a7f
glennlopez <glennlopez@gmail.com>
2016-07-08 18:00:11 -0700

    added loop

--- zyBooks/3 - Branches/3.10 - Character operations/isalpha/isalpha.cpp
+++ zyBooks/3 - Branches/3.10 - Character operations/isalpha/isalpha.cpp
@@ -3,6 +3,8 @@
 #include <string>
 using namespace std;
 
+//a simple implementation of isalpha to check if a string contains a numeric value
+
 /*
 	true if alphabetic:
 	a-z or A-Z
@@ -14,6 +16,24 @@ using namespace std;
 
 int main(){
 
+	string myString = "?";
+	bool hasNum = 0;
+
+	//enter name
+	cout << "Enter name: ";
+	getline(cin, myString);
+
+	cout << myString << endl;
+
+	//check if name has numbers
+	for(int i = 0; i < myString.length(); i++){
+
+	}
+
+
+	//password has numbers:
+
+	//password has no numbers:
 
 
 	cout << endl;
