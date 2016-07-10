f9f06861182d374d068756114061c90738813a93
glennlopez <glennlopez@gmail.com>
2016-07-10 12:22:50 -0700

    initial commit

---
+++ zyBooks/3 - Branches/3.10 - Character operations/toupper_lower/upper_lower.cpp
@@ -0,0 +1,22 @@
+//have the user chose to upper or lowercase all letters in the string
+
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+/*
+	toupper('a')  // A
+	toupper('A')  // A
+	toupper('3')  // 3
+
+	tolower('A')  // a
+	tolower('a')  // a
+	tolower('3')  // 3
+*/
+
+int main(){
+
+	cout << endl;
+	return 0;
+}
