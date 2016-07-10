ddbd864f9f449502f98c9a0c4bb9d2e973cfb1c0
glennlopez <glennlopez@gmail.com>
2016-07-10 14:10:11 -0700

    done

--- zyBooks/3 - Branches/3.10 - Character operations/toupper_lower/upper_lower.cpp
+++ zyBooks/3 - Branches/3.10 - Character operations/toupper_lower/upper_lower.cpp
@@ -13,9 +13,48 @@ using namespace std;
 	tolower('A')  // a
 	tolower('a')  // a
 	tolower('3')  // 3
+
+	common noob issues:
+	just typing toupper(myString.at(1));
+	this doesnt do anything lol, ask yourself where the toupper(myString.at(1)) is bieng assigned to
 */
 
+
 int main(){
+
+	string myString = "???";
+	unsigned int i;
+	char uChoice = '?';
+	bool upper = 0;
+
+
+	cout << "Type anything: ";
+	getline(cin, myString);
+
+	cout << "[u]pper case or [l]ower case everything (u/l): ";
+	cin >> uChoice;
+
+	if(uChoice == 'u'){
+		upper = 1;
+	}
+	else{
+		upper = 0;
+	}
+
+	//convert string choices
+	if(upper == 1){
+		for(i = 0; i < myString.length(); i++){
+			myString.at(i) = toupper(myString.at(i));
+		}
+	}
+	else{
+		for(i = 0; i < myString.length(); i++){
+			myString.at(i) = tolower(myString.at(i));
+		}
+	}
+
+	cout << myString;
+
 
 	cout << endl;
 	return 0;
