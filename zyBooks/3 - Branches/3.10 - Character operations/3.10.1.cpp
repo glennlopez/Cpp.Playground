1102294e238fd8de80b59b24c217a2409cf7353f
glennlopez <glennlopez@gmail.com>
2016-07-10 14:13:58 -0700

    solved

--- zyBooks/3 - Branches/3.10 - Character operations/3.10.1.cpp
+++ zyBooks/3 - Branches/3.10 - Character operations/3.10.1.cpp
@@ -3,7 +3,7 @@
 #include <cctype>
 using namespace std;
 
-// Set hasDigit to true if the 3-character passCode contains a digit. 
+// Set hasDigit to true if the 3-character passCode contains a digit.
 
 int main() {
    bool hasDigit = false;
@@ -13,6 +13,10 @@ int main() {
    passCode = "abc";
 
    /* Your solution goes here  */
+	for(unsigned int i = 0; i < passCode.length(); i++){
+		hasDigit = hasDigit + isdigit(passCode.at(i));
+	}
+
 
    if (hasDigit) {
       cout << "Has a digit." << endl;
