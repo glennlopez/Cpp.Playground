52988342e614cbb6ee990e872ddccb4280b9330f
glennlopez <glennlopez@gmail.com>
2016-07-10 20:12:11 -0700

    initial commit

---
+++ zyBooks/3 - Branches/3.11 - Conditional expressions/3.11.1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+	Create a conditional expression that evaluates to string "negative" if userVal is less than 0, and "positive" otherwise. Example output when userVal = -9 for the below sample program:
+	
+		-9 is negative.
+*/
+
+int main() {
+   string condStr;
+   int userVal = 0;
+
+   userVal = -9;
+
+   condStr = /* Your solution goes here  */;
+
+   cout << userVal << " is " << condStr << "." << endl;
+
+   return 0;
+}
