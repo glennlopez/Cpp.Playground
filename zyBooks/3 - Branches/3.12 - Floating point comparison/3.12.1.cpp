9a8bb2c2ba13d1901e89833e0539f79fc62b8c9a
glennlopez <glennlopez@gmail.com>
2016-07-11 20:43:34 -0700

    initial commit

---
+++ zyBooks/3 - Branches/3.12 - Floating point comparison/3.12.1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+/*
+	Write an expression that will cause the following code to print "Equal" if the value of sensorReading is "close enough" to targetValue. Otherwise, print "Not equal".
+*/
+
+int main() {
+   double targetValue = 0.3333;
+   double sensorReading = 0.0;
+
+   sensorReading = 1.0/3.0;
+
+   if (/* Your solution goes here  */) {
+      cout << "Equal" << endl;
+   }
+   else {
+      cout << "Not equal" << endl;
+   }
+
+   return 0;
+}
