994941d50c574bf60848432de5a5662c49de2a39
glennlopez <glennlopez@gmail.com>
2016-07-12 18:47:38 -0700

    inirial commit

---
+++ zyBooks/4 - Loops/4.3 - While examples/4.3.2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Given positive integer numInsects, write a while loop that prints that number doubled without reaching 100. Follow each number with a space. After the loop, print a newline. Ex: If numInsects = 8, print:
+	
+	8 16 32 64
+*/
+
+int main() {
+   int numInsects = 0;
+
+   numInsects = 8; // Must be >= 1
+
+   /* Your solution goes here  */
+
+   return 0;
+}
