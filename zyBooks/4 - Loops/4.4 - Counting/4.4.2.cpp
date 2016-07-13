313702621630ba37110739f4173a420d320109bc
glennlopez <glennlopez@gmail.com>
2016-07-12 20:04:00 -0700

    fixed words

--- zyBooks/4 - Loops/4.4 - Counting/4.4.2.cpp
+++ zyBooks/4 - Loops/4.4 - Counting/4.4.2.cpp
@@ -3,9 +3,10 @@ using namespace std;
 
 /*
 	Re-type the following and run, note incorrect behavior. Then fix errors in the code, which should print numStars asterisks.
-	while (numPrinted != numStars) {
-	   cout << "*";
-	} 
+	
+			while (numPrinted != numStars) {
+			   cout << "*";
+			}
 */
 
 int main() {
@@ -16,6 +17,7 @@ int main() {
    numPrinted = 1;
 
    /* Your solution goes here  */
+
 
    cout << endl;
 
