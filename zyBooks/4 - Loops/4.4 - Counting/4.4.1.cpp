65feb928a15ff4352b1e554802ef954306cf1d9d
glennlopez <glennlopez@gmail.com>
2016-07-12 19:19:50 -0700

    initial commit

---
+++ zyBooks/4 - Loops/4.4 - Counting/4.4.1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Write a while loop that prints 1 to userNum, using the variable i. Follow each number (even the last one) by a space. Assume userNum is positive. Ex: userNum = 4 prints:
+		1 2 3 4
+*/
+
+int main() {
+   int userNum = 0;
+   int i = 0;
+
+   userNum = 4;    // Assume positive
+
+   /* Your solution goes here  */
+
+   cout << endl;
+
+   return 0;
+}
---
+++ zyBooks/4 - Loops/4.4 - Counting/4.4.2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Re-type the following and run, note incorrect behavior. Then fix errors in the code, which should print numStars asterisks.
+	while (numPrinted != numStars) {
+	   cout << "*";
+	} 
+*/
+
+int main() {
+   int numStars = 0;
+   int numPrinted = 0;
+
+   numStars = 12;
+   numPrinted = 1;
+
+   /* Your solution goes here  */
+
+   cout << endl;
+
+   return 0;
+}
