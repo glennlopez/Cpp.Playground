9f67b581f4e66ac9bd782552a8437b9a887fb6ed
glennlopez <glennlopez@gmail.com>
2016-07-13 16:06:39 -0700

    update

---
+++ zyBooks/4 - Loops/4.5 - For loops/4.5.3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+/*
+
+	Write code that prints: userNum ... 2 1 Blastoff! Your code should contain a for loop. Print a newline after each number and after Blastoff!. Ex: userNum = 3 outputs:
+	
+		3
+		2
+		1
+		Blastoff!
+
+*/
+
+int main() {
+   int userNum = 0;
+   int i = 0;
+
+   userNum = 3;
+
+   /* Your solution goes here  */
+
+   return 0;
+}
