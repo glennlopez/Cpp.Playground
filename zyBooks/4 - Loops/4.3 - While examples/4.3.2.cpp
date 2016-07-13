25e74ec0d7375756cc6cc38c25d14c1070bd495e
glennlopez <glennlopez@gmail.com>
2016-07-12 19:14:25 -0700

    solved

--- zyBooks/4 - Loops/4.3 - While examples/4.3.2.cpp
+++ zyBooks/4 - Loops/4.3 - While examples/4.3.2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 /*
 	Given positive integer numInsects, write a while loop that prints that number doubled without reaching 100. Follow each number with a space. After the loop, print a newline. Ex: If numInsects = 8, print:
-	
+
 	8 16 32 64
 */
 
@@ -13,6 +13,12 @@ int main() {
    numInsects = 8; // Must be >= 1
 
    /* Your solution goes here  */
+	while(numInsects < 100){
+		cout << numInsects << " ";
+		numInsects = numInsects * 2;
+	}
+
+	cout << endl;
 
    return 0;
 }
