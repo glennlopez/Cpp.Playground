056f932a94382010ddedb43e6ccd7b5d45e9be0e
glennlopez <glennlopez@gmail.com>
2016-07-12 18:44:28 -0700

    solved

--- zyBooks/4 - Loops/4.2 - While Loops/4.2.3.cpp
+++ zyBooks/4 - Loops/4.2 - While Loops/4.2.3.cpp
@@ -3,14 +3,13 @@ using namespace std;
 
 /*
 
+	Write a while loop that prints userNum divided by 2 (integer division) until reaching 1. Follow each number by a space. Example output for userNum = 20:
+		20 10 5 2 1
 
-Write a while loop that prints userNum divided by 2 (integer division) until reaching 1. Follow each number by a space. Example output for userNum = 20:
-	20 10 5 2 1
 
+	Note: These activities may test code with different test values. This activity will perform four tests, with userNum = 20, then with userNum = 1, then with userNum = 0, then with userNum = -1. See How to Use zyBooks.
 
-Note: These activities may test code with different test values. This activity will perform four tests, with userNum = 20, then with userNum = 1, then with userNum = 0, then with userNum = -1. See How to Use zyBooks.
-
-Also note: If the submitted code has an infinite loop, the system will stop running the code after a few seconds, and report "Program end never reached." The system doesn't print the test case that caused the reported message.
+	Also note: If the submitted code has an infinite loop, the system will stop running the code after a few seconds, and report "Program end never reached." The system doesn't print the test case that caused the reported message.
 
 */
 
@@ -19,7 +18,20 @@ int main() {
 
    userNum = 20;
 
-   /* Your solution goes here  */
+
+	/* WRONG
+	for(unsigned int i = 0; i < userNum; i++){
+		cout << userNum << " ";
+		userNum = userNum / 2;
+	}
+	*/
+
+	/* Your solution goes here  */
+	while(userNum > 0){
+		cout << userNum << " ";
+		userNum = userNum / 2;
+	}
+
 
    cout << endl;
 
