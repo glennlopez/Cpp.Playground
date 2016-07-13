ad195da4351d09ce88438e9b143f955c716c6819
glennlopez <glennlopez@gmail.com>
2016-07-13 16:05:48 -0700

    initial

---
+++ zyBooks/4 - Loops/4.5 - For loops/4.5.2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Write a for loop that prints: 1 2 .. userNum. Print a space after each number, including after the last number. Ex: userNum = 4 prints:
+	1 2 3 4
+*/
+
+int main() {
+   int userNum = 0;
+   int i = 0;
+
+   userNum = 4;
+
+   /* Your solution goes here  */
+
+   cout << endl;
+
+   return 0;
+}
