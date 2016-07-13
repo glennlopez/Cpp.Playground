b8a9c31af11dc9b27d4096c4eb48755ee32fad2b
glennlopez <glennlopez@gmail.com>
2016-07-12 18:16:31 -0700

    initial commit

---
+++ zyBooks/4 - Loops/4.2 - While Loops/4.2.3.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+/*
+
+
+Write a while loop that prints userNum divided by 2 (integer division) until reaching 1. Follow each number by a space. Example output for userNum = 20:
+	20 10 5 2 1
+
+
+Note: These activities may test code with different test values. This activity will perform four tests, with userNum = 20, then with userNum = 1, then with userNum = 0, then with userNum = -1. See How to Use zyBooks.
+
+Also note: If the submitted code has an infinite loop, the system will stop running the code after a few seconds, and report "Program end never reached." The system doesn't print the test case that caused the reported message.
+
+*/
+
+int main() {
+   int userNum = 0;
+
+   userNum = 20;
+
+   /* Your solution goes here  */
+
+   cout << endl;
+
+   return 0;
+}
