7657a53ca4508b72a3f9ec91696b2dcf9e618b72
glennlopez <glennlopez@gmail.com>
2016-07-14 21:21:54 -0700

    new algo

--- project_euler/3.cpp
+++ project_euler/3.cpp
@@ -9,24 +9,14 @@ using namespace std;
 
 int main(){
 
-	unsigned long long i, peek;
-	vector<int> factors;
-	peek = 13195;
+	unsigned long long targetNum;
+	unsigned int i;
 
-	for(i = 1; i <= (peek/2); i++){
-		if(!(peek % i)){ //FIX ME----------------
-			if(1){
-				factors.push_back(i);
-			}
-		}
-	}
-
-	for(i = 0; i < factors.size(); i++){
-		cout << factors.at(i) << " ";
-	}
-
-
-
+	/*
+		- incremently divide targetNum starting at 1 then 2, 3 etc.. till % is 0
+		- store the new value at targetNum if there is no remainder
+		- incremently divide the new targetNum again till i reaches target num
+	*/
 
 	cout << endl;
 	return 0;
