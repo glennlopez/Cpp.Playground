c90906f4bb9bab1182714c6ff98117e051e50e97
glennlopez <glennlopez@gmail.com>
2016-07-17 19:51:17 -0700

    ??

--- project_euler/3.cpp
+++ project_euler/3.cpp
@@ -1,23 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 using namespace std;
 
 /*
 	Problem: https://projecteuler.net/problem=3
 	The prime factors of 13195 are 5, 7, 13 and 29.
+	ie: 5 * 7 * 13 * 29
 	What is the largest prime factor of the number 600851475143 ?
 */
 
 int main(){
 
-	unsigned long long targetNum;
-	unsigned int i;
 
-	/*
-		- incremently divide targetNum starting at 1 then 2, 3 etc.. till % is 0
-		- store the new value at targetNum if there is no remainder
-		- incremently divide the new targetNum again till i reaches target num
-	*/
 
 	cout << endl;
 	return 0;
