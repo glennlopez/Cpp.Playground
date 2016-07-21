ba1e34579cbe6ac9183b1668ed9a171566d6e382
glennlopez <glennlopez@gmail.com>
2016-07-20 17:37:24 -0700

    added fermat example

--- project_euler/lib_primeFermat.cpp
+++ project_euler/lib_primeFermat.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 /*
-	How Fermats Primal Test works:
-	
+	Fermats Primal Test:
+	is "P" prime?
+
+	using: (a^P - a)
+
+	P = number to test
+	a = all of the numbers between 1 and P (these all must be divisible by P to pass)
+
+
+	example: P = 5
+	1^5 - 1 = 0
+	2^5 - 2 = (32 - 2) = (30 /5) = 6
+	3^5 - 3 = (243 - 3) = (240 /5) = 48
+	4^5 - 4 = (1024 - 4) = (1020 /5) =  204
+	5^5 - 5 = (3125 - 5) = (3120 /5) = 624
+
+
+
+
 
 */
 
 int main(){
+
+	//FIXME: convert this into a function lib after
 
 	cout << endl;
 	return 0;
