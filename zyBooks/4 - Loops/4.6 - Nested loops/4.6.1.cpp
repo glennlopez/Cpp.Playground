db9796c4f9a7a7130de6218062c87654512c40c2
glennlopez <glennlopez@gmail.com>
2016-07-26 21:28:09 -0700

    initial commit

---
+++ zyBooks/4 - Loops/4.6 - Nested loops/4.6.1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Print numbers 0, 1, 2, ..., userNum as shown, with each number indented by that number of spaces. For each printed line, print the leading spaces, then the number, and then a newline. Hint: Use i and j as loop variables (initialize i and j explicitly). Note: Avoid any other spaces like spaces after the printed number. Ex: userNum = 3 prints:
+	0
+	 1
+	  2
+	   3
+
+*/
+
+int main() {
+   int userNum  = 0;
+   int i = 0;
+   int j = 0;
+
+   /* Your solution goes here  */
+
+   return 0;
+}
