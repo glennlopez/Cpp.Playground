9582fe8c735e793ee1ed9bc18832cefbead4f918
glennlopez <glennlopez@gmail.com>
2016-07-26 21:28:09 -0700

    initial commit

---
+++ zyBooks/4 - Loops/4.6 - Nested loops/4.6.2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Given numRows and numCols, print a list of all seats in a theater. Rows are numbered, columns lettered, as in 1A or 3E. Print a space after each seat, including after the last. Ex: numRows = 2 and numCols = 3 prints:
+	1A 1B 1C 2A 2B 2C
+*/
+
+int main() {
+   int numRows = 2;
+   int numCols = 3;
+
+   // Note: You'll need to define more variables
+
+   /* Your solution goes here  */
+
+   cout << endl;
+
+   return 0;
+}
