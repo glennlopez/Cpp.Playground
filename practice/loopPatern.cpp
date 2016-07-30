2cc813018e3ffa9616b64dad102c5659d2c47fd1
glennlopez <glennlopez@gmail.com>
2016-07-29 23:48:07 -0700

    nightly update

--- practice/loopPatern.cpp
+++ practice/loopPatern.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 int main(){
 
+	int x, y;
+	int i, j;
+
+	x = 5;
+	y = 3;
+
+	for(i = 1; i <= y; i++){
+		for(j = 1; j <=x; j++){
+			cout << "*";
+		}
+		cout << endl;
+	}
+
 	cout << endl;
 	return 0;
 }
