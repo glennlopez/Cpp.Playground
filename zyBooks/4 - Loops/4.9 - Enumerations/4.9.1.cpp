04e0cf2de8355f89398c4f63dd210c4a7bbf3e11
glennlopez <glennlopez@gmail.com>
2016-07-31 16:51:07 -0700

    init

---
+++ zyBooks/4 - Loops/4.9 - Enumerations/4.9.1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Print either "Fruit", "Drink", or "Unknown" (followed by a newline) depending on the value of userItem. Print "Unknown" (followed by a newline) if the value of userItem does not match any of the defined options. For example, if userItem is GR_APPLES, output should be:
+	Fruit
+*/
+
+int main() {
+   enum GroceryItem {GR_APPLES, GR_BANANAS, GR_JUICE, GR_WATER};
+
+   GroceryItem userItem = GR_APPLES;
+
+   /* Your solution goes here  */
+
+   return 0;
+}
