93269ff72496291c1ac69d1cf856c1111532b8f0
glennlopez <glennlopez@gmail.com>
2016-07-30 20:20:19 -0700

    init 1

---
+++ zyBooks/4 - Loops/4.8 - Break and continue/4.8.1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+	"Simon Says" is a memory game where "Simon" outputs a sequence of 10 characters (R, G, B, Y) and the user must repeat the sequence. Create a for loop that compares the two strings starting from index 0. For each match, add one point to userScore. Upon a mismatch, exit the loop using a break statement. Ex: The following patterns yield a userScore of 4:
+
+		simonPattern: R, R, G, B, R, Y, Y, B, G, Y
+		userPattern:  R, R, G, B, B, R, Y, B, G, Y
+
+*/
+
+int main() {
+   string simonPattern;
+   string userPattern;
+   int userScore = 0;
+   int i = 0;
+
+   userScore = 0;
+   simonPattern = "RRGBRYYBGY";
+   userPattern  = "RRGBBRYBGY";
+
+   /* Your solution goes here  */
+
+   cout << "userScore: " << userScore << endl;
+
+   return 0;
+}
