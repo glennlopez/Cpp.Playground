7f93f1aecfaf80eb4cf2885e555f990c0a05052d
glennlopez <glennlopez@gmail.com>
2016-07-31 17:50:25 -0700

    added

--- practice/for_loop_binary.cpp
+++ practice/for_loop_binary.cpp
@@ -16,4 +16,35 @@ int pow(int num, int power);
 
 int main(){
 
+	int bits, i , j, ceiling;
+	bool bin;
+
+	bin = 0;
+	bits = 2;
+	ceiling = pow(2,bits);
+
+	for(j = 1; j <= ceiling; j++){	//possible combinations
+
+		for(i = 1; i <= bits; i++){	//number of bits
+			cout << bin << " ";
+
+		}
+		cout << endl;
+
+
+	}
+
+	cout << endl;
+	return 0;
+}
+
+//Power function
+int pow(int num, int power){
+	int multiplyer = num;
+
+	for(int i = 1; i < power; i++){
+		num = num * multiplyer;
+	}
+
+	return num;
 }
