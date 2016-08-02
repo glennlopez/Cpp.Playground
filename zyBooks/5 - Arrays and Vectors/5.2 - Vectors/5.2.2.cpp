69202c1322d9e91ca6283c975a074c14b6fda25f
glennlopez <glennlopez@gmail.com>
2016-08-01 19:51:14 -0700

    init

---
+++ zyBooks/5 - Arrays and Vectors/5.2 - Vectors/5.2.2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+	Write three statements to print the first three elements of vector runTimes. Follow each with a newline. Ex: If runTime = {800, 775, 790, 805, 808}, print:
+
+		800
+		775
+		790
+*/
+
+int main() {
+   vector<int> runTimes(5);
+
+   // Populate vector
+   runTimes.at(0) = 800;
+   runTimes.at(1) = 775;
+   runTimes.at(2) = 790;
+   runTimes.at(3) = 805;
+   runTimes.at(4) = 808;
+
+   /* Your solution goes here  */
+
+   return 0;
+}
