8ba9ac42d3494640495edcfbb0d2a48cf5372dc4
glennlopez <glennlopez@gmail.com>
2016-08-01 20:00:27 -0700

    init

---
+++ project_euler/5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/*
+	https://projecteuler.net/problem=5
+	2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+	What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+*/
+
+int main(){
+
+
+
+	cout << endl;
+	return 0;
+}
