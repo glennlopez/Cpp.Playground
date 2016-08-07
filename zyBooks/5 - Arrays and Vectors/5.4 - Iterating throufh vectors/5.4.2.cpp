4c6d22ea55005186d1cdef5dfa009fe990c7f4a9
glennlopez <glennlopez@gmail.com>
2016-08-06 20:04:52 -0700

    init

---
+++ zyBooks/5 - Arrays and Vectors/5.4 - Iterating throufh vectors/5.4.2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+
+	Set numMatches to the number of elements in userValues (having NUM_VALS elements) that equal matchValue. Ex: If matchValue = 2 and userValues = {2, 2, 1, 2}, then numMatches = 3.
+
+*/
+
+int main() {
+   const int NUM_VALS = 4;
+   vector<int> userValues(NUM_VALS);
+   int i = 0;
+   int matchValue = 0;
+   int numMatches = -99; // Assign numMatches with 0 before your for loop
+
+   userValues.at(0) = 2;
+   userValues.at(1) = 2;
+   userValues.at(2) = 1;
+   userValues.at(3) = 2;
+
+   matchValue = 2;
+
+   /* Your solution goes here  */
+
+   cout << "matchValue: " << matchValue << ", numMatches: " << numMatches << endl;
+
+   return 0;
+}
