3cd41c146fee4e848ed707081c86bfdca7935e43
glennlopez <glennlopez@gmail.com>
2016-08-06 20:06:07 -0700

    init

---
+++ zyBooks/5 - Arrays and Vectors/5.4 - Iterating throufh vectors/5.4.4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+	Vector testGrades contains NUM_VALS test scores. Write a for loop that sets sumExtra to the total extra credit received. Full credit is 100, so anything over 100 is extra credit. Ex: If testGrades = {101, 83, 107, 90}, then sumExtra = 8, because 1 + 0 + 7 + 0 is 8. 
+*/
+
+int main() {
+   const int NUM_VALS = 4;
+   vector<int> testGrades(NUM_VALS);
+   int i = 0;
+   int sumExtra = -9999; // Assign sumExtra with 0 before your for loop
+
+   testGrades.at(0) = 101;
+   testGrades.at(1) = 83;
+   testGrades.at(2) = 107;
+   testGrades.at(3) = 90;
+
+   /* Your solution goes here  */
+
+   cout << "sumExtra: " << sumExtra << endl;
+   return 0;
+}
