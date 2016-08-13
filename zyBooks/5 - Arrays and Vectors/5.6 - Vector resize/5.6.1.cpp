7b0d9b4b2248535ec63c9f8883594683aec4f224
glennlopez <glennlopez@gmail.com>
2016-08-13 15:14:11 -0700

    inti

---
+++ zyBooks/5 - Arrays and Vectors/5.6 - Vector resize/5.6.1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+	Assign the size of vector sensorReadings to currentSize. 
+*/
+
+int main() {
+   vector<int> sensorReadings(4);
+   int currentSize = 0;
+
+   sensorReadings.resize(10);
+
+   /* Your solution goes here  */
+
+   cout << "Number of elements: " << currentSize << endl;
+
+   return 0;
+}
