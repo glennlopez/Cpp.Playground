24e4bea2a9b62530a4bd77acb4d74378b006c63c
glennlopez <glennlopez@gmail.com>
2016-08-13 15:49:49 -0700

    solve

--- zyBooks/5 - Arrays and Vectors/5.6 - Vector resize/5.6.2.cpp
+++ zyBooks/5 - Arrays and Vectors/5.6 - Vector resize/5.6.2.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 /*
-	Resize vector countDown to have newSize elements. Populate the vector with integers {newSize, newSize - 1, ..., 1}. Ex: If newSize = 3, then countDown = {3, 2, 1}, and the sample program outputs: 
+	Resize vector countDown to have newSize elements. Populate the vector with integers {newSize, newSize - 1, ..., 1}. Ex: If newSize = 3, then countDown = {3, 2, 1}, and the sample program outputs:
 
 		3 2 1 Go!
 */
@@ -16,6 +16,12 @@ int main() {
    newSize = 3;
 
    /* Your solution goes here  */
+	countDown.resize(newSize);
+
+	for(i = 0; i < newSize; i++){
+		countDown.at(i) = newSize - i;
+	}
+
 
    for (i = 0; i < newSize; ++i) {
       cout << countDown.at(i) << " ";
