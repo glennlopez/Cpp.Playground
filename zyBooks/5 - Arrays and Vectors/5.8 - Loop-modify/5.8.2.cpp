8193b08e25e9c0eccbf887cea93924a58d2bee52
glennlopez <glennlopez@gmail.com>
2016-08-14 13:48:22 -0700

    init

---
+++ zyBooks/5 - Arrays and Vectors/5.8 - Loop-modify/5.8.2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+	Write a loop that sets newScores to oldScores shifted once left, with element 0 copied to the end. Ex: If oldScores = {10, 20, 30, 40}, then newScores = {20, 30, 40, 10}.
+*/
+
+int main() {
+   const int SCORES_SIZE = 4;
+   vector<int> oldScores(SCORES_SIZE);
+   vector<int> newScores(SCORES_SIZE);
+   int i = 0;
+
+   oldScores.at(0) = 10;
+   oldScores.at(1) = 20;
+   oldScores.at(2) = 30;
+   oldScores.at(3) = 40;
+
+   /* Your solution goes here  */
+
+   for (i = 0; i < SCORES_SIZE; ++i) {
+      cout << newScores.at(i) << " ";
+   }
+   cout << endl;
+
+   return 0;
+}
