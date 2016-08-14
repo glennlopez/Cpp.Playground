10557ca71201a5ab74a12f9e512c2a370eee4d36
glennlopez <glennlopez@gmail.com>
2016-08-14 15:36:25 -0700

    solved

--- zyBooks/5 - Arrays and Vectors/5.8 - Loop-modify/5.8.3.cpp
+++ zyBooks/5 - Arrays and Vectors/5.8 - Loop-modify/5.8.3.cpp
@@ -6,7 +6,7 @@ using namespace std;
 	Write a loop that sets each vector element to the sum of itself and the next element, except for the last element which stays the same. Be careful not to index beyond the last element. Ex:
 		Initial scores:        10, 20, 30, 40
 		Scores after the loop: 30, 50, 70, 40
-		
+
 	The first element is 30 or 10 + 20, the second element is 50 or 20 + 30, and the third element is 70 or 30 + 40. The last element remains the same.
 
 */
@@ -22,6 +22,9 @@ int main() {
    bonusScores.at(3) = 40;
 
    /* Your solution goes here  */
+	for(i = 0; i < bonusScores.size() - 1; i++){
+		bonusScores.at(i) += bonusScores.at(i + 1);
+	}
 
    for (i = 0; i < SCORES_SIZE; ++i) {
       cout << bonusScores.at(i) << " ";
