829febb1bf550802261942a4966e1472dd2c2937
glennlopez <glennlopez@gmail.com>
2016-08-14 19:12:48 -0700

    update

--- zyBooks/5 - Arrays and Vectors/5.13 - Char arrays/using_arrays.cpp
+++ zyBooks/5 - Arrays and Vectors/5.13 - Char arrays/using_arrays.cpp
@@ -7,6 +7,9 @@ int main(){
 	char uName1[] = "Glenn";
 	char uName2[SIZE];
 
+	int num1[] = {0,1,2,3,4,5,6,7,8,9};
+	char num2[] = "0123456789";
+
 	uName2[0] = 'L';
 	uName2[1] = 'o';
 	uName2[2] = 'p';
@@ -22,6 +25,18 @@ int main(){
 	cout << "uName2[6]: " << uName2[6] << endl;
 	cout << "Size of uName2: " << sizeof(uName2) << endl;
 	cout << "Size of uName1: " << sizeof(uName1) << endl;
+	cout << endl;
+	cout << "num1: " << num1 << endl;
+	cout << "num1 size: " << sizeof(num1) << endl;
+	cout << "num1[1]: " << num1[1] << endl;
+	cout << endl;
+	cout << "num2: " << num2 << endl;
+	cout << "num2 size: " << sizeof(num2) << endl;
+	cout << "num2[1]: " << num2[1] << endl;
+	cout << endl;
+	cout << "num2[1] + num2[2]: " << num2[1] + num2[2] << endl;
+	cout << "num1[1] + num1[2]: " << num1[1] + num1[2] << endl;
+
 
 	cout << endl;
 	return 0;
