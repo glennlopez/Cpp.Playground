b2fcba7a64c1baa1a0199302ed75162760e44d3a
glennlopez <glennlopez@gmail.com>
2016-08-18 21:33:56 -0700

    update

---
+++ zyBooks/5 - Arrays and Vectors/5.13 - Char arrays/null_terminator.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	char userName[] = "Glenn";
+
+	cout << "userName: " << userName << endl;
+	cout << "size of userName: " << sizeof(userName) << endl;
+	cout << "last char: " << userName[6];
+
+	cout << endl;
+	return 0;
+}
