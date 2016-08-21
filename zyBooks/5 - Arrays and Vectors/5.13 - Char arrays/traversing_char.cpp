01a9d8869120b6cc89f2b0d97187bbaa31250043
glennlopez <glennlopez@gmail.com>
2016-08-20 17:01:37 -0700

    added sizeof()

--- zyBooks/5 - Arrays and Vectors/5.13 - Char arrays/traversing_char.cpp
+++ zyBooks/5 - Arrays and Vectors/5.13 - Char arrays/traversing_char.cpp
@@ -6,12 +6,22 @@ int main(){
 	int i;
 	char uName[] = "GlennLopez";
 
+	//using null terminator
 	for(i = 0; uName[i] != '\0'; i++){
 		if(uName[i] == 'L'){
 			cout << " " << endl;
 		}
 		cout << uName[i] << endl;
 	}
+
+	cout << endl;
+
+	//using sizeof()
+	for(i = 0; i <= sizeof(uName) - 1; i++){
+		cout << uName[i];
+	}
+
+	cout << endl;
 
 	cout << endl;
 	return 0;
