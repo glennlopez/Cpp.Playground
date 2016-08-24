c05eae6ad810f28b0374944c3c1c0d5f1bdb0228
glennlopez <glennlopez@gmail.com>
2016-08-23 17:29:47 -0700

    init

--- zyBooks/5 - Arrays and Vectors/5.14 - String library functions/strcpy.cpp
+++ zyBooks/5 - Arrays and Vectors/5.14 - String library functions/strcpy.cpp
@@ -2,7 +2,29 @@
 #include <cstring>
 using namespace std;
 
+/*
+	strcpy(destStr, sourceStr)
+	Copies sourceStr (up to and including null character) to destStr.
+*/
+
 int main(){
+
+	char originalString[] = "This is a test.";
+	char copyString[20];	//<-- the size cannot be less than original string
+
+	cout << endl;
+	cout << "Length of original string: " << sizeof(originalString) << endl;
+	cout << "Length of copyString: " << sizeof(copyString) << endl;
+	cout << "Contents of copyString: " << copyString << endl;
+	cout << endl;
+
+
+	strcpy(copyString, originalString);
+
+	cout << "After copying to copyString[]" << endl;
+	cout << "Length of copyString: " << sizeof(copyString) << endl;
+	cout << "Contents of copyString: " << copyString << endl;
+
 
 	cout << endl;
 	return 0;
