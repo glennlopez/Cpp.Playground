24229ac9e8651331192d5515e7f5cdffe3b1c307
glennlopez <glennlopez@gmail.com>
2016-08-27 19:52:51 -0700

    added comments

--- zyBooks/5 - Arrays and Vectors/5.14 - String library functions/strchr.cpp
+++ zyBooks/5 - Arrays and Vectors/5.14 - String library functions/strchr.cpp
@@ -17,7 +17,9 @@ int main(){
 	cout << "Enter your first name: ";
 	cin >> myString;
 
+	//check if the string has the letter N
 	if( (strchr(myString, 'n') != NULL) || (strchr(myString, 'N') != NULL ) ){
+		//count the number of N's in the name
 		for(unsigned int i = 0; myString[i] != '\0'; i++){
 			if((myString[i] == 'n') || (myString[i] == 'N')){
 				count++;
