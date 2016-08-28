979f9392b735de8b9fc159ef8ad9ef94face510d
glennlopez <glennlopez@gmail.com>
2016-08-27 20:32:47 -0700

    init

---
+++ zyBooks/5 - Arrays and Vectors/5.14 - String library functions/strlen.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+/*
+	size_t strlen(sourceStr)
+
+	Returns number of characters in sourceStr up to, but not including, first null character. size_t is integer type.
+*/
+
+int main(){
+
+	char myString[30] = "Name";
+
+
+
+	cout << endl;
+	return 0;
+}
