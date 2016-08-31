82303dbc9472599b90d98bac2f82394877758f33
glennlopez <glennlopez@gmail.com>
2016-08-30 19:01:35 -0700

    init

---
+++ zyBooks/6 - User defined functions/6.2 - Paramaters/6.2.2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/*
+
+	Define a function PrintFeetInchShort, with int parameters numFeet and numInches, that prints using ' and " shorthand. Ex: PrintFeetInchShort(5, 8) prints:
+	5' 8"
+	Hint: Use \" to print a double quote.
+
+/*
+
+/* Your solution goes here  */
+
+int main() {
+   PrintFeetInchShort(5, 8);
+   cout << endl;
+
+   return 0;
+}
