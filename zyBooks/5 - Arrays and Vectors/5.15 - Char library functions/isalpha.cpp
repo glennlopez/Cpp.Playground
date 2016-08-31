0f3c4e2a8f5422940d3c7468d845eae4e209c27f
glennlopez <glennlopez@gmail.com>
2016-08-30 18:40:02 -0700

    update

---
+++ zyBooks/5 - Arrays and Vectors/5.15 - Char library functions/isalpha.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+/*
+	isalpha(c) -- Returns true if c is alphabetic: a-z or A-Z.
+	isalpha('A'); // Returns true
+	isalpha(myString[0]); // Returns true because 'H' is alphabetic
+	isalpha(myString[3]); // Returns false because '9' is not alphabetic
+*/
+
+int main(){
+
+	cout << endl;
+	return 0;
+}
