1a50096bfa3330aff8d519de84f0f7b4245e6330
glennlopez <glennlopez@gmail.com>
2016-09-04 14:43:05 -0700

    init1

--- REFERANCE/Array_types/char_arrays.cpp
+++ REFERANCE/Array_types/char_arrays.cpp
@@ -3,11 +3,27 @@ using namespace std;
 
 int main(){
 
-	char myString[] = "abcdefghij";
+	char A1[] = "abcdefghij"; // null terminator automaticall included
+	char A2[] = {'m', 'n', 'o', 'p', '\0'}; //null terminator explicitly included
+	char A3[] = {'x', 'y', 'z'}; //null trminator NOT included
 
-	cout << "cout << myString: " << myString << endl;
-	cout << "sizeof(myString[1]): " << sizeof(myString[1]) << endl;
-	cout << "sizeof(myString): " << sizeof(myString) << endl;
+	cout << endl;
+
+	cout << "cout << A1: " << A1 << endl;
+	cout << "sizeof(A1[1]): " << sizeof(A1[1]) << endl;
+	cout << "sizeof(A1): " << sizeof(A1) << endl;
+
+	cout << endl;
+
+	cout << "cout << A2: " << A2 << endl;
+	cout << "sizeof(A2[1]): " << sizeof(A2[1]) << endl;
+	cout << "sizeof(A2): " << sizeof(A2) << endl;
+
+	cout << endl;
+
+	cout << "cout << A3: " << A3 << endl;
+	cout << "sizeof(A3[1]): " << sizeof(A3[1]) << endl;
+	cout << "sizeof(A3): " << sizeof(A3) << endl;
 
 	cout << endl;
 	return 0;
