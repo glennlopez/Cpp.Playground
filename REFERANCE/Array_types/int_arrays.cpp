d7f6ed369fd83ef0ee5646d189ed53ccc562c61a
glennlopez <glennlopez@gmail.com>
2016-09-04 14:43:50 -0700

    changed myString to A2

--- REFERANCE/Array_types/int_arrays.cpp
+++ REFERANCE/Array_types/int_arrays.cpp
@@ -3,22 +3,24 @@ using namespace std;
 
 int main(){
 
-	int myString[] = {0,1,2,3,4,5,6,7,8,9}; // each number is 4bit in size
+	int A1[] = {0,1,2,3,4,5,6,7,8,9}; // each number is 4bit in size
 	unsigned int i, total;
 
-	//NOTE THE SIZE OF ARRAY: notice how myString[1] is size 4, and myString is size 40: 4 * 10 = 40
-	cout << "cout << myString: " << myString << endl;	//gives the memory ID if array is type int
-	cout << "sizeof(myString[1]): " << sizeof(myString[1]) << endl;
-	cout << "sizeof(myString): " << sizeof(myString) << endl;
-	cout << "real size of myString: " << (sizeof(myString)/4) << endl;
+	cout << endl;
+
+	//NOTE THE SIZE OF ARRAY: notice how A1[1] is size 4, and A1 is size 40: 4 * 10 = 40
+	cout << "cout << A1: " << A1 << endl;	//gives the memory ID if array is type int
+	cout << "sizeof(A1[1]): " << sizeof(A1[1]) << endl;
+	cout << "sizeof(A1): " << sizeof(A1) << endl;
+	cout << "real size of A1: " << (sizeof(A1)/4) << endl;
 
 	//adding int arrays
-	total = myString[0];
-	for(i = 1; i < sizeof(myString)/4; i++){
-		total += myString[i];
+	total = A1[0];
+	for(i = 1; i < sizeof(A1)/4; i++){
+		total += A1[i];
 	}
 
-	cout << "Sum of int inside myString array: " << total << endl;
+	cout << "Sum of int inside A1 array: " << total << endl;
 
 	cout << endl;
 	return 0;
