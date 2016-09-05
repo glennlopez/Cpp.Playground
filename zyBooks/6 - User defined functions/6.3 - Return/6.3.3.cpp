14d2bfdbd788241439e1bdb03e4fc6faf49b417e
glennlopez <glennlopez@gmail.com>
2016-09-05 16:48:45 -0700

    init

---
+++ zyBooks/6 - User defined functions/6.3 - Return/6.3.3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Define a function PyramidVolume with double parameters baseLength, baseWidth, and pyramidHeight, that returns as a double the volume of a pyramid with a rectangular base. Relevant geometry equations:
+
+		Volume = base area x height x 1/3
+		Base area = base length x base width. 
+		(Watch out for integer division).
+*/
+
+/* Your solution goes here  */
+
+int main() {
+   cout << "Volume for 1.0, 1.0, 1.0 is: " << PyramidVolume(1.0, 1.0, 1.0) << endl;
+   return 0;
+}
