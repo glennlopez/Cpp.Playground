5309474d7389eccc0472b3a3c48038d732be96d0
glennlopez <glennlopez@gmail.com>
2016-09-07 19:27:23 -0700

    solved

--- zyBooks/6 - User defined functions/6.5 - Functions with branch/6.5.2.cpp
+++ zyBooks/6 - User defined functions/6.5 - Functions with branch/6.5.2.cpp
@@ -11,6 +11,23 @@ using namespace std;
 */
 
 /* Your solution goes here  */
+void PrintShampooInstructions(int numCycles){
+	if(numCycles < 1){
+		cout << "Too few." << endl;
+	}
+
+	else if(numCycles > 4){
+		cout << "Too many." << endl;
+	}
+
+	else{
+		for(unsigned int i = 1; i <= numCycles; i++){
+			cout << i << ": Lather and rinse." << endl;
+		}
+		cout << "Done." << endl;
+	}
+}
+
 
 int main() {
    PrintShampooInstructions(2);
