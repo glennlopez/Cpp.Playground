a9213a6f5eee506a01c12fe9a72df396a52f4f5d
glennlopez <glennlopez@gmail.com>
2016-09-07 19:06:32 -0700

    update

---
+++ zyBooks/6 - User defined functions/6.5 - Functions with branch/6.5.2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Write a function PrintShampooInstructions(), with int parameter numCycles, and void return type. If numCycles is less than 1, print "Too few.". If more than 4, print "Too many.". Else, print "N: Lather and rinse." numCycles times, where N is the cycle number, followed by "Done.". End with a newline. Example output for numCycles = 2:
+	1: Lather and rinse.
+	2: Lather and rinse.
+	Done.
+
+	Hint: Define and use a loop variable.
+*/
+
+/* Your solution goes here  */
+
+int main() {
+   PrintShampooInstructions(2);
+
+   return 0;
+}
