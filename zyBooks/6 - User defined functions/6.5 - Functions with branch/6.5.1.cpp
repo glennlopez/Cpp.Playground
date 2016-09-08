92eef96704375509aedc8e0428292f45d01e9c9b
glennlopez <glennlopez@gmail.com>
2016-09-07 19:06:32 -0700

    update

---
+++ zyBooks/6 - User defined functions/6.5 - Functions with branch/6.5.1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Complete function PrintPopcornTime(), with int parameter bagOunces, and void return type. If bagOunces is less than 3, print "Too small". If greater than 10, print "Too large". Otherwise, compute and print 6 * bagOunces followed by "seconds". End with a newline. Example output for ounces = 7:
+
+		42 seconds
+*/
+
+void PrintPopcornTime(int bagOunces) {
+
+   /* Your solution goes here  */
+
+}
+
+int main() {
+   PrintPopcornTime(7);
+
+   return 0;
+}
