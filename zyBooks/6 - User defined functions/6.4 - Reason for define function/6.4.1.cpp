b6058a1914b986d06291f1e6c9e11b0f66fd6299
glennlopez <glennlopez@gmail.com>
2016-09-07 19:00:32 -0700

    solved

---
+++ zyBooks/6 - User defined functions/6.4 - Reason for define function/6.4.1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+/*
+	Define stubs for the functions called by the below main(). Each stub should print "FIXME: Finish FunctionName()" followed by a newline, and should return -1. Example output:
+	FIXME: Finish GetUserNum()
+	FIXME: Finish GetUserNum()
+	FIXME: Finish ComputeAvg()
+	Avg: -1
+*/
+
+/* Your solution goes here  */
+int GetUserNum(){
+	cout << "FIXME: Finish GetUserNum()";
+	cout << endl;
+	return -1;
+}
+
+int ComputeAvg(int, int){
+	cout << "FIXME: Finish ComputeAvg()";
+	cout << endl;
+	return -1;
+}
+
+int main() {
+   int userNum1 = 0;
+   int userNum2 = 0;
+   int avgResult = 0;
+
+   userNum1 = GetUserNum();
+   userNum2 = GetUserNum();
+
+   avgResult = ComputeAvg(userNum1, userNum2);
+
+   cout << "Avg: " << avgResult << endl;
+
+   return 0;
+}
