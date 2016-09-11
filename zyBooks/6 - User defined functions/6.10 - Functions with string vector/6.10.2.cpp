5219c21d5f982d43f2749ced2be0c5c82dc8c755
glennlopez <glennlopez@gmail.com>
2016-09-11 14:21:55 -0700

    init

---
+++ zyBooks/6 - User defined functions/6.10 - Functions with string vector/6.10.2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+	Complete the function to replace any period by an exclamation point. Ex: "Hello. I'm Miley. Nice to meet you." becomes:
+
+		"Hello! I'm Miley! Nice to meet you!"
+*/
+
+void MakeSentenceExcited(string& sentenceText) {
+
+   /* Your solution goes here  */
+
+}
+
+int main() {
+   string testStr;
+
+   testStr = "Hello. I'm Miley. Nice to meet you.";
+   MakeSentenceExcited(testStr);
+   cout << testStr;
+
+   return 0;
+}
