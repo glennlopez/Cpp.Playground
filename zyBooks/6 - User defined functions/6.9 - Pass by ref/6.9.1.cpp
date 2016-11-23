2fec49dc6595338dda7a98342a69409e4613825d
glennlopez <glennlopez@gmail.com>
2016-11-22 21:02:14 -0800

    nightly

--- zyBooks/6 - User defined functions/6.9 - Pass by ref/6.9.1.cpp
+++ zyBooks/6 - User defined functions/6.9 - Pass by ref/6.9.1.cpp
@@ -6,9 +6,9 @@ using namespace std;
 */
 
 /* Your solution goes here  */
-void CoordTransform(int xVal, int yVal, int &xValNew, int &yValNew){
-	xValNew = (xVal + 1) * 2;
-	yValNew = (yVal + 1) * 2;
+void CoordTransform(int xval, int yval, int &xValNew, int &yValNew){
+	xValNew = (xval + 1) * 2;
+	yValNew = (yval + 1) * 2;
 }
 
 int main() {
