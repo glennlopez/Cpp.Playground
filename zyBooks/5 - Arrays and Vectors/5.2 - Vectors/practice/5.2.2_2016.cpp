550c88454c8239d1bd70a5d0592dbacf0a5b2cb0
glennlopez <glennlopez@gmail.com>
2016-11-28 16:05:50 -0800

    solved

--- zyBooks/5 - Arrays and Vectors/5.2 - Vectors/practice/5.2.2_2016.cpp
+++ zyBooks/5 - Arrays and Vectors/5.2 - Vectors/practice/5.2.2_2016.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
+/*
+	Write three statements to print the first three elements of vector runTimes. Follow each with a newline. Ex: If runTime = {800, 775, 790, 805, 808}, print:
+		800
+		775
+		790
 
-	cout << endl;
-	return 0;
+
+	Note: These activities may test code with different test values. This activity will perform two tests, the first with a 5-element vector (vector<int> runTimes(5)), the second with a 4-element vector (vector<int> runTimes(4)). See How to Use zyBooks.
+
+	Also note: If the submitted code tries to access an invalid vector element, such as runTime.at(9) for a 5-element vector, the test may generate strange results. Or the test may crash and report "Program end never reached", in which case the system doesn't print the test case that caused the reported message.
+*/
+
+int main() {
+   vector<int> runTimes(5);
+
+   // Populate vector
+   runTimes.at(0) = 800;
+   runTimes.at(1) = 775;
+   runTimes.at(2) = 790;
+   runTimes.at(3) = 805;
+   runTimes.at(4) = 808;
+
+   /* Your solution goes here  */
+	cout << runTimes.at(0) << endl;
+	cout << runTimes.at(1) << endl;
+	cout << runTimes.at(2) << endl;
+
+
+   return 0;
 }
