dc6f5399bf11b8162c47cd9d6133383d7885f197
glennlopez <glennlopez@gmail.com>
2016-12-05 10:45:58 -0800

    prototype

--- project_euler/5.cpp
+++ project_euler/5.cpp
@@ -10,33 +10,75 @@ using namespace std;
 
 int main(){
 
-	unsigned int i;
-	unsigned int startNum, endLoop;
+	//FIXME: find a formula for loop ceiling
+	//possible algo: 2*3*4*5*6*7*8*9*10 = ceiling
 
-	startNum = (20 * 20);
-	endLoop = 0;
+	//iterate to loop cieling
 
+	//run a check to see if numbers are divisible by 1 - 10
 
-	//loop until n is divisible by numbers 1 through 10
-	while(!(endLoop)){
-		if(1/*temp*/){
+ 	long n = (1*2*3*4*5*6*7*8*9*10)/2;
+	int nCount = 0;
+	int nTarget = 10;
 
+	while(n > 0){
+		//cout << "n is " << n << endl;
+
+		if((n % 1) == 0){
+			//cout << "div by 1" << endl;
+			nCount++;
 		}
-		else{
-
+		if((n % 2) == 0){
+			//cout << "div by 2" << endl;
+			nCount++;
 		}
+		if((n % 3) == 0){
+			//cout << "div by 3" << endl;
+			nCount++;
+		}
+		if((n % 4) == 0){
+			//cout << "div by 4" << endl;
+			nCount++;
+		}
+		if((n % 5) == 0){
+			//cout << "div by 5" << endl;
+			nCount++;
+		}
+		if((n % 6) == 0){
+			//cout << "div by 6" << endl;
+			nCount++;
+		}
+		if((n % 7) == 0){
+			//cout << "div by 7" << endl;
+			nCount++;
+		}
+		if((n % 8) == 0){
+			//cout << "div by 8" << endl;
+			nCount++;
+		}
+		if((n % 9) == 0){
+			//cout << "div by 9" << endl;
+			nCount++;
+		}
+		if((n % 10) == 0){
+			//cout << "div by 10" << endl;
+			nCount++;
+		}
+
+		if(nCount == nTarget){
+			//cout << "MATCH!!!" << endl;
+			cout << n << endl;
+			//break;
+		}
+
+		//cout << "Div by " << nCount << "/" << nTarget << " numbers" << endl;
+		nCount = 0;
+		//break;
+		n--;
+
 
 	}
 
-
-
-
-	//iterate through numbers starting from 20
-		//test each iterations 1 - 20
-		//if 20 pass then:
-			//print the number that passed iterations of divisions without remainders
-		//if fail then:
-			//break; move on to the next iteration
 
 
 
