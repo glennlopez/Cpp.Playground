4c59d65271eb505c33026cf96c2be3752794cc90
glennlopez <glennlopez@gmail.com>
2016-12-05 11:04:41 -0800

    cleaner prototype

--- project_euler/5.cpp
+++ project_euler/5.cpp
@@ -10,78 +10,28 @@ using namespace std;
 
 int main(){
 
-	//FIXME: find a formula for loop ceiling
-	//possible algo: 2*3*4*5*6*7*8*9*10 = ceiling
-
-	//iterate to loop cieling
-
-	//run a check to see if numbers are divisible by 1 - 10
-
  	long n = (1*2*3*4*5*6*7*8*9*10)/2;
 	int nCount = 0;
 	int nTarget = 10;
 
 	while(n > 0){
-		//cout << "n is " << n << endl;
-
-		if((n % 1) == 0){
-			//cout << "div by 1" << endl;
-			nCount++;
-		}
-		if((n % 2) == 0){
-			//cout << "div by 2" << endl;
-			nCount++;
-		}
-		if((n % 3) == 0){
-			//cout << "div by 3" << endl;
-			nCount++;
-		}
-		if((n % 4) == 0){
-			//cout << "div by 4" << endl;
-			nCount++;
-		}
-		if((n % 5) == 0){
-			//cout << "div by 5" << endl;
-			nCount++;
-		}
-		if((n % 6) == 0){
-			//cout << "div by 6" << endl;
-			nCount++;
-		}
-		if((n % 7) == 0){
-			//cout << "div by 7" << endl;
-			nCount++;
-		}
-		if((n % 8) == 0){
-			//cout << "div by 8" << endl;
-			nCount++;
-		}
-		if((n % 9) == 0){
-			//cout << "div by 9" << endl;
-			nCount++;
-		}
-		if((n % 10) == 0){
-			//cout << "div by 10" << endl;
-			nCount++;
+		int d = 1;
+		while(d < 11){
+			if((n % d) == 0){
+				nCount++;
+			}
+			d++;
 		}
 
 		if(nCount == nTarget){
-			//cout << "MATCH!!!" << endl;
 			cout << n << endl;
-			//break;
 		}
 
-		//cout << "Div by " << nCount << "/" << nTarget << " numbers" << endl;
 		nCount = 0;
-		//break;
 		n--;
 
 
 	}
-
-
-
-
 
 
 	cout << endl;
