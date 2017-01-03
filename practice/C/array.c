ff6ca87dd318c8d1bb00f2d82b636921537ed316
glennlopez <glennlopez@gmail.com>
2017-01-03 13:31:31 -0800

    added C folder

---
+++ practice/C/array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+	int myArray[8] = {5,10,12,11,2,-1,0,120};
+	int num;
+
+	printf("The largest number in the array is: ");
+
+	num = myArray[1];
+
+
+	printf(" \n");
+	return 0;
+}
