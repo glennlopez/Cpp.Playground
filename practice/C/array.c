0ad9478bb8a95d0b3cb44f42a75a1c2c42687b63
glennlopez <glennlopez@gmail.com>
2017-01-03 13:48:42 -0800

    finisehd excersize

--- practice/C/array.c
+++ practice/C/array.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
+//https://www.tutorialspoint.com/c_standard_library/c_function_printf.htm
 
 int main(){
 
-	int myArray[8] = {5,10,12,11,2,-1,0,120};
-	int num;
+	int myArray[8] = {5,190,-12,1011,2,-1,0,900};
+	int largestNum, smallestNum, i;
 
-	printf("The largest number in the array is: ");
+	smallestNum = 9999;
+	largestNum = -9999;
 
-	num = myArray[1];
+	for(i = 0; i < 8; i++){
+		if(myArray[i] > largestNum){
+			largestNum = myArray[i];
+		}
+		if(myArray[i] < smallestNum){
+			smallestNum = myArray[i];
+		}
+	}
 
+	printf("The largest number in the array is: %i ", largestNum); printf(" \n");	//new line
+	printf("The smallestNum number in the array is: %i ", smallestNum);
 
-	printf(" \n");
+	printf(" \n");	//new line
 	return 0;
 }
