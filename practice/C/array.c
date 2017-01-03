6fda1421a5cb441c64aee048063c20193f25ffc6
glennlopez <glennlopez@gmail.com>
2017-01-03 13:56:35 -0800

    added comments

--- practice/C/array.c
+++ practice/C/array.c
@@ -6,8 +6,9 @@ int main(){
 	int myArray[8] = {5,190,-12,1011,2,-1,0,900};
 	int largestNum, smallestNum, i;
 
-	smallestNum = 9999;
-	largestNum = -9999;
+	//vars set to max val to avoid iteration bugs
+	smallestNum = 99999999;	//set to max val
+	largestNum = -99999999;	//set to max val
 
 	for(i = 0; i < 8; i++){
 		if(myArray[i] > largestNum){
