10a315c69b777b9d64f858826680bbebd56f170d
glennlopez <glennlopez@gmail.com>
2017-01-03 17:21:06 -0800

    finished example

--- practice/C/strings.c
+++ practice/C/strings.c
@@ -4,6 +4,20 @@
 
 int main(){
 
+	int i, count;
+	char myArray[12] = {'H','e','l','l','o',' ','W','o','r','l','d','\0'};
+
+	count = 0;
+	for(i = 0; i < 12; i++){
+		printf("%c", myArray[i]);
+		if(myArray[i] == 'l'){
+			count++;
+		}
+	} printf("\n");
+
+
+
+	printf("The letter L shows up this many times: %i\n", count);
 
 
 	printf("\n");
