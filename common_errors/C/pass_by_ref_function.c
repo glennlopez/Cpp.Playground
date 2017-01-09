76d3973b8e09e2e586de6feeb579b0078e3c7476
glennlopez <glennlopez@gmail.com>
2017-01-09 01:16:08 -0800

    set primer

--- common_errors/C/pass_by_ref_function.c
+++ common_errors/C/pass_by_ref_function.c
@@ -1,8 +1,17 @@
+// https://www.youtube.com/watch?v=wAmq8eIkdI8
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+	I keep forgetting how to do passbyreference/adderess function arguments.
+	I think part of the reason is that I do not use this as often as I do with
+	Pass by value. It is most likely the result of me not yet understanding
+	how to properly manage memeory in C/CPP
+*/
+
 //pritotype
 void passByValue(int);
+void passByAddress(int*);
 
 int main(){
 
@@ -11,6 +20,13 @@ int main(){
 	return 0;
 }
 
+
+
+
 void passByValue(int i){
 	i = 99;
+}
+
+void passByAddress(int *i){
+	*i = 64;
 }
