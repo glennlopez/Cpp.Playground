79c2d94ffd08ed8ec286d0bcc1048d4718aa4421
glennlopez <glennlopez@gmail.com>
2017-01-09 15:35:51 -0800

    added gets and puts

---
+++ practice/C/gets_puts.c
@@ -0,0 +1,13 @@
+//https://www.youtube.com/watch?v=yvpCxgV3AZo
+#include <stdio.h>
+
+int main(){
+	char catsName[50];
+	char catsFood[25];
+	char sentense[75];
+
+	gets("What is the cats name?");
+
+	printf("\n");
+	return 0;
+}
