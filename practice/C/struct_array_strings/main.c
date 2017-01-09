45b27119ceeea69a741f1b1a0c0ded79c3a0b2d8
glennlopez <glennlopez@gmail.com>
2017-01-08 22:15:10 -0800

    added strings in array in structs

---
+++ practice/C/struct_array_strings/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "struct.h"	//include the struct.h
+
+int main(){
+	
+
+
+
+	printf("\n");
+	return 0;
+}
---
+++ practice/C/struct_array_strings/struct.h
@@ -0,0 +1,5 @@
+// class vs struct: https://www.youtube.com/watch?v=aw7r4VMmagE
+
+struct name{
+
+};
