fcb01c631a2db66357716d431ffebe7b500478b5
glennlopez <glennlopez@gmail.com>
2017-01-09 16:32:55 -0800

    passing struct to function

--- practice/C/struct_array_strings/main.c
+++ practice/C/struct_array_strings/main.c
@@ -1,12 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "struct.h"	//include the struct.h
+#include "books.h"
+
+//prototypes
+void askUsrNum(int*);
+void indexBooks(int);
 
 int main(){
-	
+	int usrNum;
+	struct book science[usrNum];
 
+	askUsrNum(&usrNum);	//ask user how many books to catalog
+	indexBooks(usrNum);	//record the books using struct
+
+	//output the book data
+
+	printf("Title: %s\n", science[0].title);
+	printf("Author: %s\n", science[0].author);
+	printf("BIN: %i\n", science[0].bin);
+	printf("userNum: %i\n", usrNum);
 
 
 	printf("\n");
 	return 0;
+}
+
+
+/**************************
+Functions
+**************************/
+void askUsrNum(int *usrNum){
+	printf("How many books are you cataloging: ");
+	scanf("%i", usrNum);
+}
+
+void indexBooks(int usrNum){
+
+	int i = 0;
+	int limit = (usrNum -1);
+	for(i = 0; i <= limit; i++){
+		printf("Book %i Title: ", i+1);
+		scanf("%s", science[i].title);
+
+		printf("Book %i Author: ", i+1);
+		scanf("%s", science[i].author);
+
+		printf("Book %i BIN: ", i+1);
+		scanf("%i", &science[i].bin);
+
+		printf("\n");	//newline
+	}
 }
