8cc3d7dacb5913ccf3da568a5ff188f26f19f9de
glennlopez <glennlopez@gmail.com>
2017-01-11 17:22:16 -0800

    update

--- practice/C/struct_typedef.c
+++ practice/C/struct_typedef.c
@@ -3,17 +3,25 @@
 struct player{
 	unsigned int xpos;
 	unsigned int ypos;
-	unsigned int score;
+	unsigned int health;
 };
 
-typedef struct player NPC;
-typedef struct player PC;
+typedef struct player PlayerType;
 
 int main(){
-	NPC space_ship={12,500,0};
-	PC player_1={0,500,600};
+	//boundery x = 100 y = 100 health = 100
+	PlayerType player1 = {50,1,100};
+	PlayerType npc[4];
 
-	printf("Player 1 score: %i\n",player_1.score);
+	//npc[1] = {10, 20, 100}; //<-- doesnt work
+	npc[0].xpos = 10;
+	npc[0].ypos = 20;
+	npc[0].health = 100;
+
+
+	printf("Player 1 health: %i\n",player1.health);
+	printf("NPC 0 position x= %i, y= %i.\n",npc[0].xpos, npc[0].ypos);
+	printf("NPC 1 position x= %i, y= %i.\n",npc[1].xpos, npc[1].ypos);
 
 	printf("\n");
 	return 0;
