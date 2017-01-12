b8050bfefdf74faa186d8bb86be7ccb86256563a
glennlopez <glennlopez@gmail.com>
2017-01-11 17:22:16 -0800

    update

---
+++ practice/C/finite_state_machine.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+	This is a prototype for a simple finite state
+	machine that is to be implemented on a microcontroller.
+	
+		Detect odd numbers of 1's and output a ON if odd
+		and OFF if even.
+*/
+
+int main(){
+
+	printf("\n");
+	return 0;
+}
