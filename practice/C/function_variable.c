8aa2df4df155f7d02c7cb84d17944870e2af11d3
glennlopez <glennlopez@gmail.com>
2017-01-13 01:59:49 -0800

    finished practice sesstion

--- practice/C/function_variable.c
+++ practice/C/function_variable.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
-int maint(){
+//sum function
+int add(int a, int b){
+	return a + b;
+}
 
 
+int main(){	int sum;
+
+//STEPS//
+	/*1*/	int (*p)(int, int);	//define the function pointer
+	/*2*/	p = &add;				//point p to address of add 
+	/*3*/	sum = (*p)(10,5);		//call the pointer function
+
+	printf("Sum is: %i\n", sum);
 	printf("\n");
 	return 0;
 }
