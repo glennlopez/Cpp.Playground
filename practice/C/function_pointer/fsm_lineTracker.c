6180556df86b0314247c5c64aff78064a5aaa6df
glennlopez <glennlopez@gmail.com>
2017-01-14 14:49:05 -0800

    added inline struct definition

--- practice/C/function_pointer/fsm_lineTracker.c
+++ practice/C/function_pointer/fsm_lineTracker.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 
-int mani(){
+struct dataBank{
+	void (*pFunk)(void);		//output
+	unsigned int delay;		//delay
+	unsigned int next[3];	//next states
+
+}; typedef const struct dataBank machine;
+
+
+
+
+/**************
+	MAIN
+**************/
+int main(){
 
 	printf("\n");	//newline
 	return 0;
 }
+
+
+
+
+
+/**************
+	FUNCTION
+**************/
