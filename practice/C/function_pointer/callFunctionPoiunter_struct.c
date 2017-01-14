79ca3767f557e62dd6beb9411ba80b65190a6ec4
glennlopez <glennlopez@gmail.com>
2017-01-13 19:33:32 -0800

    added practice session

--- practice/C/function_pointer/callFunctionPoiunter_struct.c
+++ practice/C/function_pointer/callFunctionPoiunter_struct.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 
+struct data{
+	void (*pFunc)(void);
+	int (*add)(int a, int b);
+}; typedef const struct data machine;
+
+
+
+
+
 int main(){
 
 
 	printf("\n");
 	return 0;
 }
+
+
+
+
+/*************
+	FUNCTION
+**************/
