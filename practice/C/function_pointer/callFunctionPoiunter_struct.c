a7918b152f9920ab53f21d020834c60ab2fe711b
glennlopez <glennlopez@gmail.com>
2017-01-13 20:23:29 -0800

    added structs

--- practice/C/function_pointer/callFunctionPoiunter_struct.c
+++ practice/C/function_pointer/callFunctionPoiunter_struct.c
@@ -1,15 +1,32 @@
 #include <stdio.h>
 
-struct data{
-	void (*pFunc)(void);
-	int (*add)(int a, int b);
-}; typedef const struct data machine;
+//struct printing data
+struct printData{
+	void (*dataFunc)(void);
+	int output;
+}; typedef const struct printData output;
 
+//struct calculating data
+struct calcData{
+	int (*calcFunc)(int, int);
+	int input[2];
+}; typedef const struct calcData computer;
 
+//prototypes
+void hWorld(void);
+void name(void);
 
+int add(int, int);
+int multiply(int, int);
+
+output Option[2] = {
+	{&hWorld, 0},
+	{&name, 1}
+};
 
 
 int main(){
+
 
 
 	printf("\n");
@@ -22,3 +39,16 @@ int main(){
 /*************
 	FUNCTION
 **************/
+void hWorld(void){
+	printf("Hello world!\n");
+}
+void name(void){
+	printf("What is your name again?\n");
+}
+
+int add(int a, int b){
+	return a + b;
+}
+int multiply(int a, int b){
+	return (a * b);
+}
