2ab4c12a4fb0005e11f89c2431c1ed06dee60f07
glennlopez <glennlopez@gmail.com>
2017-01-14 16:30:41 -0800

    change pFunc pointer name to *out

--- practice/C/function_pointer/fsm_lineTracker.c
+++ practice/C/function_pointer/fsm_lineTracker.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 struct dataBank{
-	void (*pFunk)(void);		//output a function
+	void (*out)(void);		//output a function
 	unsigned int delay;		//delay param
 	unsigned int next[4];	//4 possible next states
 
 }; typedef const struct dataBank machine;
 
-//definitions for next states
-#define CEN 0	//center
-#define LEF 1	//left
-#define RIG 2	//right
+//state definitions
+unsigned int cState;	//current state
+#define CEN 0			//center state
+#define LEF 1			//left state
+#define RIG 2			//right state
 
 //function prototypes
 void Center(void);
@@ -18,11 +19,11 @@ void Left(void);
 void Right(void);
 
 machine FSM[3] = {
-	//output		delay		next states
-	//							00		01		10		11
+//(*output)		delay		{next states}
 	{&Center,	1,			{RIG,	LEF,	RIG,	CEN}},
 	{&Left,		1,			{LEF,	CEN,	RIG,	CEN}},
 	{&Right,		1,			{RIG,	LEF,	CEN,	CEN}}
+	//							00		01		10		11
 };
 
 
@@ -30,6 +31,9 @@ machine FSM[3] = {
 	MAIN
 **************/
 int main(){
+	cState = CEN;	//set initial state to CENTER
+
+	(FSM[0].out)();
 
 	printf("\n");	//newline
 	return 0;
