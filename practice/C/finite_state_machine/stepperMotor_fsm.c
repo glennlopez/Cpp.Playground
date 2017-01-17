1167c31274d8905c8a2e7e3b965e48f356ceb4c0
glennlopez <glennlopez@gmail.com>
2017-01-17 11:21:18 -0800

    added struct

--- practice/C/finite_state_machine/stepperMotor_fsm.c
+++ practice/C/finite_state_machine/stepperMotor_fsm.c
@@ -1,7 +1,50 @@
 #include <stdio.h>
 
+//struct datatype definition
+struct fsmData{
+	void (*out)();
+	unsigned int output;
+	unsigned int wait;
+	unsigned int next[4];
+}; typedef struct fsmData machine;
+
+//prototype
+
+//FSM Data stream
+machine MotorFSM[10] = {
+	//
+	{},
+
+	{},
+	{},
+	{},
+
+	{},
+	{},
+	{},
+
+	{},
+	{},
+	{}
+};
+
+
+
+
+/***************
+	MAIN
+****************/
 int main(){
+
+
 
 	printf("\n");	//newline
 	return 0;
 }
+
+
+
+
+/***************
+	FUNCTION
+****************/
