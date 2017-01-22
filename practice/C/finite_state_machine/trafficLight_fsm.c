ef32d8d46a4238f376b01dd8bb75ab6d660dea90
glennlopez <glennlopez@gmail.com>
2017-01-21 18:45:32 -0800

    added synopsis

--- practice/C/finite_state_machine/trafficLight_fsm.c
+++ practice/C/finite_state_machine/trafficLight_fsm.c
@@ -1,3 +1,14 @@
+/*
+	3 inputs: 	2 car sensors, 1 pedestrian
+	8 outputs:	3 west, 3 south, 2 pedestrian
+
+	pedestrian sensor is push and hold - presense sense
+	car sensors are also push and hold - presense sense
+
+	west & south lights must both be red before walk light can be green
+	walk light should be red while cars have green or yellow light
+
+*/
 #include <stdio.h>
 
 int main(){
