51d07e8942a625e50369b67222bc882941b17573
glennlopez <glennlopez@gmail.com>
2017-01-21 20:38:37 -0800

    added documentation

--- practice/C/finite_state_machine/trafficLight_fsm.c
+++ practice/C/finite_state_machine/trafficLight_fsm.c
@@ -8,11 +8,54 @@
 	west & south lights must both be red before walk light can be green
 	walk light should be red while cars have green or yellow light
 
+	PORT B: 6 Traffic lights (output)
+	PORT F: 2 Pedestrian lights (output)
+	PORT E: 3 Sensors (input)
+
+	~ uC OUTPUTS ~
+	South/North: 	PB0 - PB2
+	West/East: 		PB3 - PB5
+
+	Walk Light:		PF1
+	Dont Walk:		PF3
+
+
+	~ uC INPUTS ~
+	pedestrian Detector:		PE2
+	South/North Detector:	PE1
+	West/East Detector:		PE0
+
+
+
 */
 #include <stdio.h>
 
+struct PiDataSet{
+	void (*out)(void);
+	unsigned int delay;
+	unsigned int next[4];
+}; typedef const struct PiDataSet FSM;
+
+FSM PiTraffic[7] = {
+
+};
+
+/************
+	MAIN
+************/
 int main(){
+
+	while(1){			//inf loop
+
+	}
 
 	printf("\n");		//newline
 	return 0;
 }
+
+
+
+
+/*******************
+	PI FUNCTIONS
+********************/
