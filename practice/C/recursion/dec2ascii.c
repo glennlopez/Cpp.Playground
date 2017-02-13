484d1a67dae431dce09882ff37bad29312dea3d0
glennlopez <glennlopez@gmail.com>
2017-02-12 22:17:19 -0800

    nightly

--- practice/C/recursion/dec2ascii.c
+++ practice/C/recursion/dec2ascii.c
@@ -3,13 +3,27 @@ void UART_ConvertUDec(unsigned long n);
 unsigned char String[10];
 
 int main(){
-	UART_ConvertUDec(12345);
+	unsigned long param = 12345;
+	UART_ConvertUDec(param);
 
-	printf("%c\n", String[0]);
-	printf("%c\n", String[1]);
-	printf("%c\n", String[2]);
-	printf("%c\n", String[3]);
-	printf("%c\n", String[4]);
+	//debug: display each char
+	printf("0: %c\n", String[0]);
+	printf("1: %c\n", String[1]);
+	printf("2: %c\n", String[2]);
+	printf("3: %c\n", String[3]);
+	printf("4: %c\n", String[4]);
+	printf("5: %c\n", String[5]);
+	printf("6: %c\n", String[6]);
+	printf("7: %c\n", String[7]);
+
+	//debug: count where '\0' is
+	//FIXME: Finish this
+
+	//debug: print input
+	printf("Input(lu): %lu\n", param);
+	//debug: print string
+	printf("Output(s): %s\n", String);
+
 
 
 	printf("\n");		//newline
@@ -27,12 +41,16 @@ void UART_ConvertUDec(unsigned long n){
 		tempN = tempN / 10;
 	}
 
+	//debug: place null char at the end
+	String[5] = '\0';
+
 	//convert dec to ascii
 	unsigned long i;
 	for(i = 0; i < count; i++){
-		String[i] = (n % 10) + 0x30;	//convert dec to ascii
+		String[i] = (n % 10) + 0x30;	//place ascii in Strings array
 		n = n / 10;	//decrement to the next digit in n
 	}
 
-	printf("count: %lu\n", count);
+	//debug: print count
+	printf("Number of digits: %lu\n", count);
 }
