f428f9ac5a514658432d7aac3245779d47b9f6ac
glennlopez <glennlopez@gmail.com>
2017-06-02 20:17:27 -0700

    added day 8

---
+++ HackerRank/Tutorials/30 Days of Code/Day 8: Maps/main.cpp
@@ -0,0 +1,33 @@
+//https://www.hackerrank.com/challenges/30-dictionaries-and-maps
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+/* SAMPLE INPUT
+3
+sam 99912222
+tom 11122222
+harry 12299933
+sam
+edward
+harry
+*/
+
+/* SAMPLE OUTPUT
+sam=99912222
+Not found
+harry=12299933
+*/
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+
+
+    return 0;
+}
