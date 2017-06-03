d89fe2cba4972969aeb3b559ee42c3e8acdf8f66
glennlopez <glennlopez@gmail.com>
2017-06-03 16:08:51 -0700

    added new challenge

---
+++ HackerRank/Core CS/Algorithms/Warmup/VeryBigSum.cpp
@@ -0,0 +1,28 @@
+//https://www.hackerrank.com/challenges/a-very-big-sum
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+/* SAMPLE INPUT
+  5
+  1000000001 1000000002 1000000003 1000000004 1000000005
+*/
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int arr_i = 0;arr_i < n;arr_i++){
+       cin >> arr[arr_i];
+    }
+    return 0;
+}
+
+
+/* SAMPLE OUTPUT
+  5000000015
+*/
