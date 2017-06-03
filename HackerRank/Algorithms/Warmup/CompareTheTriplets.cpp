1a572bf92b1fb2331544f3065d68ec4ee3c03145
glennlopez <glennlopez@gmail.com>
2017-06-03 11:11:17 -0700

    cleaned up code

--- HackerRank/Algorithms/Warmup/CompareTheTriplets.cpp
+++ HackerRank/Algorithms/Warmup/CompareTheTriplets.cpp
@@ -16,10 +16,12 @@ int main() {
     int a1;
     int a2;
     cin >> a0 >> a1 >> a2;
+
     int b0;
     int b1;
     int b2;
     cin >> b0 >> b1 >> b2;
+    
     vector < int > result = solve(a0, a1, a2, b0, b1, b2);
     for (ssize_t i = 0; i < result.size(); i++) {
         cout << result[i] << (i != result.size() - 1 ? " " : "");
