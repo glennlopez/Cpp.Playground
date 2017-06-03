5e13012e1adf570ed63ca12269db86bf6b4348cf
glennlopez <glennlopez@gmail.com>
2017-06-03 16:41:22 -0700

    solved challenge

--- HackerRank/Core CS/Algorithms/Warmup/VeryBigSum.cpp
+++ HackerRank/Core CS/Algorithms/Warmup/VeryBigSum.cpp
@@ -16,9 +16,17 @@ int main(){
     int n;
     cin >> n;
     vector<int> arr(n);
+
+    long long total = 0;
+
     for(int arr_i = 0;arr_i < n;arr_i++){
        cin >> arr[arr_i];
+       total += arr[arr_i];
     }
+
+    cout << total;
+
+
     return 0;
 }
 
