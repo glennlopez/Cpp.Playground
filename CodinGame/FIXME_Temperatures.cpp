39463127200ca4adb720783d66594932bce055ed
glennlopez <glennlopez@gmail.com>
2017-06-04 14:55:11 -0700

    reset

--- CodinGame/FIXME_Temperatures.cpp
+++ CodinGame/FIXME_Temperatures.cpp
@@ -1,4 +1,3 @@
-//https://www.codingame.com/ide/puzzle/temperatures
 #include <iostream>
 #include <string>
 #include <vector>
@@ -14,42 +13,11 @@ int main()
 {
     int n; // the number of temperatures to analyse
     cin >> n; cin.ignore();
-    //string temps; // the n temperatures expressed as integers ranging from -273 to 5526
-    //getline(cin, temps);
-    int usrNum, temps, pos, neg;
-    vector<int> posV, negV;
-
-    //sort pos and negative values
-    neg = -999999;
-    pos = 999999;
-    for(unsigned int i = 0; i < n; i++){
-      cin >> usrNum;
-      if(usrNum < 0){
-        negV.push_back(usrNum); //used as a temp buffer
-        if(negV[i] > neg){
-          neg = negV[i];
-        }
-      }
-      if(usrNum > 0){
-        posV.push_back(usrNum); //used as a temp buffer
-        if(posV[i] < pos){
-          pos = posV[i];
-        }
-      }
-    }
-
-
-
-    //test output
-    cout << " ----- " << endl;
-    cout << "Negative: " << neg << endl;
-    cout << "Positive: " << pos << endl;
-
-    //cerr << temps << endl;
+    string temps; // the n temperatures expressed as integers ranging from -273 to 5526
+    getline(cin, temps);
 
     // Write an action using cout. DON'T FORGET THE "<< endl"
     // To debug: cerr << "Debug messages..." << endl;
 
-
-    //cout << "result" << endl;
+    cout << "result" << endl;
 }
