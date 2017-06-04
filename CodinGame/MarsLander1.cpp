bea85a9ce61102e29a398f42fe6d771549c3b93b
glennlopez <glennlopez@gmail.com>
2017-06-03 22:47:26 -0700

    solved mars lander challenge

---
+++ CodinGame/MarsLander1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+int main()
+{
+    int surfaceN; // the number of points used to draw the surface of Mars.
+    cin >> surfaceN; cin.ignore();
+    for (int i = 0; i < surfaceN; i++) {
+        int landX; // X coordinate of a surface point. (0 to 6999)
+        int landY; // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
+        cin >> landX >> landY; cin.ignore();
+    }
+
+    // game loop
+    while (1) {
+        int X;
+        int Y;
+        int hSpeed; // the horizontal speed (in m/s), can be negative.
+        int vSpeed; // the vertical speed (in m/s), can be negative.
+        int fuel; // the quantity of remaining fuel in liters.
+        int rotate; // the rotation angle in degrees (-90 to 90).
+        int power; // the thrust power (0 to 4).
+        cin >> X >> Y >> hSpeed >> vSpeed >> fuel >> rotate >> power; cin.ignore();
+
+        // Write an action using cout. DON'T FORGET THE "<< endl"
+        // To debug: cerr << "Debug messages..." << endl;
+
+        /* SOLUTION */
+        if(Y < 1850){
+            if(vSpeed < -39){
+                cout << "0 4" << endl;
+            }
+            else{
+                cout << "0 0" << endl;
+            }
+        }
+        else{
+            cout << "0 2" << endl;
+        }
+
+        /* END OF SOLUTION */
+
+        // 2 integers: rotate power. rotate is the desired rotation angle (should be 0 for level 1), power is the desired thrust power (0 to 4).
+        //cout << "0 3" << endl;
+    }
+}
