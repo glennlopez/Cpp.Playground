1d30f5cedd241d5af0635aed8d21a94bfc150f45
glennlopez <glennlopez@gmail.com>
2017-06-03 22:49:46 -0700

    added temp challenge

---
+++ CodinGame/Temperatures.cpp
@@ -0,0 +1,26 @@
+//https://www.codingame.com/ide/puzzle/temperatures
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+int main()
+{
+    int n; // the number of temperatures to analyse
+    cin >> n; cin.ignore();
+    string temps; // the n temperatures expressed as integers ranging from -273 to 5526
+    getline(cin, temps);
+
+    cerr << temps << endl;
+
+    // Write an action using cout. DON'T FORGET THE "<< endl"
+    // To debug: cerr << "Debug messages..." << endl;
+
+    cout << "result" << endl;
+}
