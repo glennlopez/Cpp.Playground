e5f2c7105321e4a29b0dac7335a51211968ac6ed
glennlopez <glennlopez@gmail.com>
2017-06-04 12:12:53 -0700

    added comments

--- HackerRank/Tutorials/30 Days of Code/Day 9: Recursion/main.cpp
+++ HackerRank/Tutorials/30 Days of Code/Day 9: Recursion/main.cpp
@@ -6,6 +6,13 @@
 #include <algorithm>
 using namespace std;
 
+//sample input: 3
+//sample output: 6
+/*
+  Write a factorial function that takes a positive integer,
+  as a parameter and prints the result of  ( factorial).
+*/
+
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
