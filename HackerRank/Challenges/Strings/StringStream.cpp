44c791eb8b9f97d6f7d511877543b4409491fc61
glennlopez <glennlopez@gmail.com>
2017-06-05 14:53:42 -0700

    update

--- HackerRank/Challenges/Strings/StringStream.cpp
+++ HackerRank/Challenges/Strings/StringStream.cpp
@@ -1,10 +1,18 @@
+//https://www.hackerrank.com/challenges/c-tutorial-stringstream
+//help: https://www.hackerrank.com/challenges/c-tutorial-stringstream/forum
 #include <sstream>
 #include <vector>
 #include <iostream>
+#include <string>
 using namespace std;
 
 vector<int> parseInts(string str) {
    // Complete this function
+   vector<int> buffer;
+   stringstream ss(str);
+
+
+   return buffer;
 }
 
 int main() {
