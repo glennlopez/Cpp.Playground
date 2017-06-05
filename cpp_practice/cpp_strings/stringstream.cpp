f82c05b89ecdac2a028c2a8fd247292088e6ff88
glennlopez <glennlopez@gmail.com>
2017-06-04 21:24:46 -0700

    added examples

--- cpp_practice/cpp_strings/stringstream.cpp
+++ cpp_practice/cpp_strings/stringstream.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include <vector>
+#include <sstream>
 using namespace std;
 
 int main(){
+  string myString = "1,2,3,4";
+  int myNum = 1234;
+  char myChar = 'c';
 
+  stringstream ss;
+
+  ss << myString;
+  cout << ss.str() << endl;
+
+  cout << endl;
   return 0;
 }
