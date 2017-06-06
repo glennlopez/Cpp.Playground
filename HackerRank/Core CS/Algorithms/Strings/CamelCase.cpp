36bfb978cc96d9a41ba008747c0ee8f066b88dc3
glennlopez <glennlopez@gmail.com>
2017-06-06 12:09:25 -0700

    added challenege

---
+++ HackerRank/Core CS/Algorithms/Strings/CamelCase.cpp
@@ -0,0 +1,41 @@
+//https://www.hackerrank.com/challenges/camelcase
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+/* SAMPLE INPUT
+  saveChangesInTheEditor
+*/
+
+
+int main(){
+    string s;
+    cin >> s;
+    return 0;
+}
+
+
+/* SAMPLE OUTPUT
+  5
+*/
