1977b425ea327f46a2223cab2ef0fda3290a422c
glennlopez <glennlopez@gmail.com>
2017-06-07 15:56:48 -0700

    solved

--- HackerRank/Core CS/Algorithms/Warmup/MinMaxSum.cpp
+++ HackerRank/Core CS/Algorithms/Warmup/MinMaxSum.cpp
@@ -7,10 +7,35 @@ using namespace std;
 // Sample input: 1 2 3 4 5
 
 int main() {
-    vector<int> arr(5);
-    for(int arr_i = 0; arr_i < 5; arr_i++){
-       cin >> arr[arr_i];
+
+  //My Solution: add the vector elements and subtract arr[i] to find min and max
+
+  long long total, largest, smallest;
+  vector<int> arr(5);
+
+  total = 0;
+
+  for(int arr_i = 0; arr_i < 5; arr_i++){
+    cin >> arr[arr_i];
+    total = total + arr[arr_i]; //get total
+  }
+
+  largest = total - arr[4];
+  smallest = total - arr[4];
+
+  for(int i = 0; i < 5; i++){
+    if(largest < total - arr[i]){ //find the largest
+      largest = total - arr[i];
     }
+    if(smallest > total - arr[i]){ //find the smallest
+      smallest = total - arr[i];
+    }
+  }
+
+
+  cout << smallest << " " << largest << endl;
+
+
     return 0;
 }
 
