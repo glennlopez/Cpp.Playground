af2868646a2b9b075dd34431847de1066edcc8a2
glennlopez <glennlopez@gmail.com>
2017-06-07 14:07:50 -0700

    added new challenge

---
+++ HackerRank/Core CS/Algorithms/Warmup/MinMaxSum.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+/* https://www.hackerrank.com/challenges/mini-max-sum
+  Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+*/
+
+// Sample input: 1 2 3 4 5
+
+int main() {
+    vector<int> arr(5);
+    for(int arr_i = 0; arr_i < 5; arr_i++){
+       cin >> arr[arr_i];
+    }
+    return 0;
+}
+
+// Sample output: 10 14
