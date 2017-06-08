984bbd4064e411322cf8e07371eba76a71ecd404
glennlopez <glennlopez@gmail.com>
2017-06-08 16:47:25 -0700

    added problems

---
+++ HackerRank/Core CS/Algorithms/Warmup/DiagonalDifference.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector< vector<int> > a(n,vector<int>(n));
+    for(int a_i = 0;a_i < n;a_i++){
+       for(int a_j = 0;a_j < n;a_j++){
+          cin >> a[a_i][a_j];
+       }
+    }
+    return 0;
+}
