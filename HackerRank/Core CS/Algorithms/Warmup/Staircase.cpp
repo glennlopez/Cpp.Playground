37f52cdf37c8d30e74c1b6cbf453f1c9333585a4
glennlopez <glennlopez@gmail.com>
2017-06-07 22:31:30 -0700

    added challenge

---
+++ HackerRank/Core CS/Algorithms/Warmup/Staircase.cpp
@@ -0,0 +1,35 @@
+//https://www.hackerrank.com/challenges/staircase
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+/*
+  Observe that its base and height are both equal to , and the image is drawn using # symbols and spaces. The last line is not preceded by any spaces.
+
+  Write a program that prints a staircase of size .
+*/
+
+
+int main(){
+    int n;
+    cin >> n;
+    return 0;
+}
+
+
+/*
+  Sample input: 6
+
+  Sample output:
+
+    #
+    ##
+    ###
+    ####
+    #####
+    ######
+
+*/
