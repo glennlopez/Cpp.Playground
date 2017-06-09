e83a2fc3f6bfbfb0f722a248a343d7a4c3261352
glennlopez <glennlopez@gmail.com>
2017-06-08 19:35:51 -0700

    added problem

---
+++ HackerRank/Core CS/Data Structure/Arrays/ArraysDS.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int arr_i = 0;arr_i < n;arr_i++){
+       cin >> arr[arr_i];
+    }
+    return 0;
+}
