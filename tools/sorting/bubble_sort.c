eea90ea49115b20a4ec8bf9bd91f26eeaf46db09
Glenn Lopez <glennlopez@gmail.com>
2017-06-13 22:22:19 +0000

    update

--- tools/sorting/bubble_sort.c
+++ tools/sorting/bubble_sort.c
@@ -5,12 +5,24 @@
 //int usrArr[6] = {5,2,1,3,6,4};
 //int usrArr[6] = {1,2,3,4,5,6};
 //int usrArr[6] = {5,2,1,3,4,6};
-int usrArr[6] = {6,2,5,3,4,1};
+//int usrArr[6] = {6,2,5,3,4,1};
+int usrArr[6] = {6,5,4,3,2,1};
 int totalSteps = 0;
 
 #define ARRLEN(arr) ( sizeof(arr) / sizeof(arr[0]))
 
 int main(){
+    
+    //print original array
+    printf("Before: ");
+    for(int i = 0; i < ARRLEN(usrArr); i++){
+        printf("%i ", usrArr[i]);
+    }printf("\n");
+    printf(" After: ");
+    
+    
+    
+    
     
     //PSEUDOCODE
         //set swap counter to a non-zero value
@@ -29,7 +41,7 @@ int main(){
     do{
         swapCounter = 0;
         
-        for(int i = 0; i < ARRLEN(usrArr) -1; i++){
+        for(int i = 0, n = ARRLEN(usrArr); i < n -1; i++){
         
             if(usrArr[i] > usrArr[i+1]){
                 buffer = usrArr[i+1];       totalSteps++;
@@ -38,22 +50,22 @@ int main(){
                 swapCounter++;
             }
                                             totalSteps++;   //min arraylen(arr) steps
-        }
+        } 
         
     }while(swapCounter != 0);
     //END BUBBLE SORT ALGORYTHM
     
     
     
-    //print array
+    
+    
+    //print sorted array
     for(int i = 0; i < ARRLEN(usrArr); i++){
         printf("%i ", usrArr[i]);
     }
     
     printf("\n-------\n");
-    printf("Swap Counter: %i\n", swapCounter);
-    printf("Arr[0]: %i\n", usrArr[0]);
-    printf("Arr size: %i\n", (int)ARRLEN(usrArr));
-    printf("Total Steps: %i\n", totalSteps);
+    printf("Array size: %i\n", (int)ARRLEN(usrArr));
+    printf("Total steps to sort: %i\n", totalSteps);
     return 0;
 }
