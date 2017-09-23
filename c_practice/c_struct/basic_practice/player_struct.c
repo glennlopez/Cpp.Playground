7b9c1390b9584fda3c4fa3c43105026646577c3b
Glenn Lopez <glennlopez@gmail.com>
2017-09-22 21:21:19 -0700

    practice

--- c_practice/c_struct/basic_practice/player_struct.c
+++ c_practice/c_struct/basic_practice/player_struct.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+typedef struct {
+    char *pName;
+    int pHealth;
+} player;
+
 int main(){
 
-  return 0;
+    player NPC_ROB;
+
+    NPC_ROB.pName = "Rob the cat";
+    NPC_ROB.pHealth = 100;
+
+    for(int i = 0; i != '\0'; i++){
+    
+    }
+
+
+
+    return 0;
 }
