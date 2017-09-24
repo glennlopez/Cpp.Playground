332a2476b4808defcb0d41943a5a6f5ea9f678a9
Glenn Lopez <glennlopez@gmail.com>
2017-09-23 17:22:17 -0700

    practice added

---
+++ c_practice/c_cmdArgs/basic_arg.c
@@ -0,0 +1,11 @@
+//tutorial at: https://www.youtube.com/watch?time_continue=39&v=eDI5Wqjq2a0
+#include "stdio.h"
+#include "cs50.h"
+
+//argc = argument count
+//argv = argument vector
+int main(int argc, string argv[]){
+
+    printf("\n");
+    return 0;
+}
