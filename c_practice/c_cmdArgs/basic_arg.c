525e9697b72a3be1ac1f7f914412bd1874e4cc89
Glenn Lopez <glennlopez@gmail.com>
2017-09-24 15:53:02 -0700

    finished exmaple

--- c_practice/c_cmdArgs/basic_arg.c
+++ c_practice/c_cmdArgs/basic_arg.c
@@ -1,10 +1,35 @@
 //tutorial at: https://www.youtube.com/watch?time_continue=39&v=eDI5Wqjq2a0
-#include "stdio.h"
-#include "cs50.h"
+            //https://www.youtube.com/watch?time_continue=1380&v=2zPEHYoiyfc
+#include <stdio.h>
+#include <cs50.h>
 
 //argc = argument count
 //argv = argument vector
-int main(int argc, string argv[]){
+int main(int argc, char* argv[]){   //<-- using char*
+//int main(int argc, string argv[]){ //<-- using string
+
+    //USING char*
+
+    if(argc == 2 ){
+        printf("Hello ");
+        for(int i = 0; argv[1][i] != '\0'; i++){
+            printf("%c", argv[1][i]);
+        }
+    }
+
+
+    //USING string
+    /*
+    if(argc == 2){
+        printf("Hello, %s", argv[1]);
+    }
+    */
+
+
+    //else statement if no arguments are entered
+    else{
+        printf("hello world \n");
+    }
 
     printf("\n");
     return 0;
