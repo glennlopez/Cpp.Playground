42c27751b81f0a871cef7af9b2c5483b61d85b4d
Glenn Lopez <glennlopez@gmail.com>
2017-10-15 00:28:35 -0700

    FIXME: program the algorthm

---
+++ tools/math_func/decToBin.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+Algorythm: Divide the Decimal by 2 until the number being divided is less than 2,
+keeping track of any remainders
+
+    12/2 = 6    (0)
+    6/2 = 3     (0)
+    3/2 = 1     (1) <-- 3/2 = 1 w/ 1 remainder
+    1/2 = 1     (1) <-- 1/2 = 1 w/ 1 remainder + 1 is less than 2 so stop looping
+
+ Now, we simply need to write out the remainder in the reverse order — 1100. So, 12 in decimal system is represented as 1100 in binary.
+*/
+
+int main(){
+
+    //FIXME: Program the decimal to binary algorythm above
+
+    printf("\n");
+    return 0;
+}
