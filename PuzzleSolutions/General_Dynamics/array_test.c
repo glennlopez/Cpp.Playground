fe6665efa91f1b5d45330528bc705a68bd16c5fd
Glenn Lopez <glennlopez@gmail.com>
2018-03-24 11:47:43 -0500

    2d array practice

---
+++ PuzzleSolutions/General_Dynamics/array_test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    const int MAX_Y = 2;    //this will change
+    const int MAX_X = 3;
+
+    int myArr[MAX_Y][MAX_X] = {
+        {1,2,3},
+        {4,5,6}
+    };
+
+
+
+    printf("\n");
+    return 0;
+}
