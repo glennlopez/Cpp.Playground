8448467051279a594f9f48ae66ead52c7f6a018d
Glenn Lopez <glennlopez@gmail.com>
2018-05-30 15:32:31 -0500

    finished challenge

--- Lynda.com/Advanced C Programming/4. Arrays and Structure/structure_function.c
+++ Lynda.com/Advanced C Programming/4. Arrays and Structure/structure_function.c
@@ -13,11 +13,38 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+    char name[30];
+    int age;
+} person;
+
+void fillStruct(person *p);
+void displayStruct(person *p);
+int num;
+
 
 int main()
 {
-    
 
+    // prompt user number of person
+    printf("How many people: ");
+    scanf("%d", &num);
+
+    // allocate to heap - retain data till free
+    person *ptr;
+    ptr = malloc(num * sizeof(person));
+
+    // Fill struct data
+    fillStruct(ptr);
+
+    // Display struct data
+    displayStruct(ptr);
+
+    // free allocated memory 
+    free(ptr);
 
     printf("\n");
     return 0;
@@ -32,6 +59,21 @@ int main()
 
 
 // fill the struct
+void fillStruct(person *p)
+{
+    for(int i = 0; i < num ; i++)
+    {
+        printf("Enter Name: ");
+        scanf("%s", p[i].name);
+    }
+}
 
 
 // display the stuct
+void displayStruct(person *p)
+{
+    for(int i = 0; i < num; i++)
+    {
+        printf("Name: %s\n", p[i].name);
+    }
+}
