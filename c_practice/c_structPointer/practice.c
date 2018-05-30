d40caaa95add9110fd00a5e4eb822df7659cf1f8
Glenn Lopez <glennlopez@gmail.com>
2018-05-30 15:00:23 -0500

    update

--- c_practice/c_structPointer/practice.c
+++ c_practice/c_structPointer/practice.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-int main(){
+/*
+    Accessing structure member through pointer using dynamic memory allocation
+*/
+
+typedef struct
+{
+    int age;
+    float weight;
+    char name[30];
+} person;
+
+int main()
+{
+    int num = 2;
+    person *ptr;
+
+    // Place struct data to heap memory
+    ptr = malloc(num * sizeof(person));
+
+    // Prompt user for data
+    for(int i = 0; i < num; i++)
+    {
+        printf("Enter age: ");
+        //scanf("%d", &(*(ptr+i)).age);
+        //scanf("%d", &(ptr+i)->age);
+        scanf("%d", &ptr[i].age);
+    }
+
+    // Display data
+    for(int i = 0; i < num; i++)
+    {
+        printf("Age: %d\n", ptr[i].age);
+    }
+
 
     printf("\n");
     return 0;
