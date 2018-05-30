daf17b1c1a86edf8ed32382dbf173c779ac70b6b
Glenn Lopez <glennlopez@gmail.com>
2018-05-30 15:00:23 -0500

    update

---
+++ Lynda.com/Advanced C Programming/4. Arrays and Structure/structure_function.c
@@ -0,0 +1,37 @@
+ /*
+    Challenge Objective:
+        Code two functions that:
+            [] Fill struct data with data
+            [] Displays struct data
+        Struct members must have:
+            [] Name
+            [] Age
+
+    Prompt the user to enter thier name and age.
+    Then display the results
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    
+
+
+    printf("\n");
+    return 0;
+}
+
+
+
+
+/****************
+    SUBROUTINES
+*****************/
+
+
+// fill the struct
+
+
+// display the stuct
