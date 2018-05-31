43914c5ff08c85c81718a0dbe949533d1a4d63db
Glenn Lopez <glennlopez@gmail.com>
2018-05-31 14:51:51 -0500

    practice update

--- c_practice/c_struct/nested_struct/practice.c
+++ c_practice/c_struct/nested_struct/practice.c
@@ -21,14 +21,22 @@ typedef struct{
 
 // prototypes
 void titleUnderline(petInfo);
+void displayPetInfo(petInfo param);
+void populatePetInfo(petInfo param);
 
 
 
 int main()
 {
-    // Define a petInfo structure variable
-    petInfo pet[2];
+    // TODO: Ask user how many pets
 
+    // Number of pets
+    int petAmt = 1;
+
+    // Define a petInfo structure variable
+    petInfo pet[petAmt];
+
+    /*
     // Assigning values to petInfo variables
     strcpy(pet[0].name, "Garfield");
     strcpy(pet[0].type, "Cat");
@@ -37,16 +45,24 @@ int main()
     pet[0].cost.medical = 140.11;
     pet[0].cost.license = 60.74;
     pet[0].cost.other = 11.75;
+    */
 
-    // DEBUG - Display all values 
-    printf("\n%s\n", pet[0].name);
-    titleUnderline(pet[0]);
-    printf("Pet type: %s\n", pet[0].type);
-    printf("Age: %i\n", pet[0].age);
-    printf("Age: %i\n", pet[0].age);
+    // Populate pet values
+    for(int i = 0; i < petAmt; i++)
+    {
+        printf("Pet #%i\n", i+1);
+        populatePetInfo(pet[i]);
+        printf("\n");
+    }
 
+    // Display pet values
+    for(int i = 0; i < petAmt; i++)
+    {
+        displayPetInfo(pet[i]);
+    }
+    
 
-    // Calculate anual cost for the pet
+    // TODO:Calculate anual cost for the pet
 
     printf("\n");
     return 0;
@@ -55,7 +71,7 @@ int main()
 
 
 
-// subroutines
+// Subroutines
 void titleUnderline(petInfo param)
 {
     int i;
@@ -64,4 +80,38 @@ void titleUnderline(petInfo param)
         printf("-");
     }
     printf("\n");
+}
+
+void displayPetInfo(petInfo param)
+{
+    printf("\n%s\n", param.name);
+    titleUnderline(param);
+    printf("pet type: %s\n", param.type);
+    printf("Age: %i\n", param.age);
+    printf("Food: $%1.2f\n", param.cost.food);
+    printf("Medical: $%1.2f\n", param.cost.medical);
+    printf("License: $%1.2f\n", param.cost.license);
+    printf("Other Costs: $%1.2f\n", param.cost.other);
+}
+
+void populatePetInfo(petInfo param)
+{   
+    char petName[32];
+    char petType[32];
+    printf("Enter pets name: ");
+    scanf("%s", petName);
+    printf("Enter pet type: ");
+    scanf("%s", petType);
+    strcpy(param.name, petName);
+    strcpy(param.type, petType);
+    printf("Enter pet age: ");
+    scanf("%d", &param.age);
+    printf("Food cost: ");
+    scanf("%lf", &param.cost.food);
+    printf("Medical cost: ");
+    scanf("%lf", &param.cost.medical);
+    printf("License cost: ");
+    scanf("%lf", &param.cost.license);
+    printf("Other cost: ");
+    scanf("%lf", &param.cost.other);
 }
