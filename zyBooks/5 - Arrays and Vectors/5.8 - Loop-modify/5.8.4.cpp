a2451cffaa84c8d51c434a62eb5ae77ff4a124e3
Becka Henry <36617288+beckahenryl@users.noreply.github.com>
2019-03-13 15:24:01 -0700

    resolved

--- zyBooks/5 - Arrays and Vectors/5.8 - Loop-modify/5.8.4.cpp
+++ zyBooks/5 - Arrays and Vectors/5.8 - Loop-modify/5.8.4.cpp
@@ -2,33 +2,31 @@
 #include <vector>
 using namespace std;
 
-/*
-	Double any element's value that is less than minVal. Ex: If minVal = 10, then dataPoints = {2, 12, 9, 20} becomes {4, 12, 18, 20}.
-*/
-
 int main() {
-   const int NUM_POINTS = 4;
+   int maxVal;
+   int NUM_POINTS;
+   unsigned int i;
+   cin >> maxVal;
+   cin >> NUM_POINTS;
    vector<int> dataPoints(NUM_POINTS);
-   int minVal = 0;
-   int i = 0;
 
-   dataPoints.at(0) = 2;
-   dataPoints.at(1) = 12;
-   dataPoints.at(2) = 9;
-   dataPoints.at(3) = 20;
-
-   minVal = 10;
-
-   for (i = 0; i < NUM_POINTS; ++i)
-   {
-      if (dataPoints[i] < minVal) 
-      {
-         dataPoints[i] = dataPoints[i] * 2;
-      }
+   for (i = 0; i < dataPoints.size(); ++i) {
+      cin >> dataPoints.at(i);
    }
 
+     
+     for (i = 0; i < NUM_POINTS; ++i)
+   {
+      if (dataPoints[i] > maxVal) 
+      {
+         dataPoints[i] = dataPoints[i] - 4;
+      }
+      else{
+         dataPoints[i] = dataPoints[i];
+         }
+   }
 
-   for (i = 0; i < NUM_POINTS; ++i) {
+   for (i = 0; i < dataPoints.size(); ++i) {
       cout << dataPoints.at(i) << " " ;
    }
    cout << endl;
