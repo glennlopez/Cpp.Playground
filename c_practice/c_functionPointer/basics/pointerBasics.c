a8beeecd66cc7a9fdef9a741bf19ed09d3d8d0a6
Glenn Lopez <glennlopez@gmail.com>
2022-03-30 10:54:53 -0500

    added practice session

---
+++ c_practice/c_functionPointer/basics/pointerBasics.c
@@ -0,0 +1,17 @@
+//https://www.youtube.com/watch?v=zuegQmMdy8M
+//https://www.youtube.com/watch?v=q24-QTbKQS8
+#include <stdio.h>
+
+struct Person
+{
+    char name[260];
+    int age;
+};
+
+typedef struct Person person;
+
+int main()
+{
+    
+
+}
