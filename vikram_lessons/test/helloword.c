fbe47c5625bd49920ed3c5adda1163c1569210c7
Glenn Lopez <glennlopez@gmail.com>
2023-12-09 10:43:15 -0600

    added co pilot test

--- vikram_lessons/test/helloword.c
+++ vikram_lessons/test/helloword.c
@@ -12,4 +12,34 @@ int main() {
     printf("Final number is: %i \n", i);
     printf("This is a test for LiveShare\n");
     printf("Another test...\n");
+    
+    // calculate factorial of an integer - copilot test
+    int n = 10;
+    int factorial = 1;
+    for (int j = 1; j <= n; j++)
+    {
+        factorial *= j;
+    }
+    printf("Factorial of %i is %i\n", n, factorial);
+    
+    //check if a number is prime or not - copilot test
+    int num = factorial;
+    int isPrime = 1;
+    for (int k = 2; k < num; k++)
+    {
+        if (num % k == 0)
+        {
+            isPrime = 0;
+            break;
+        }
+    }
+    if (isPrime)
+    {
+        printf("%i is a prime number\n", num);
+    }
+    else
+    {
+        printf("%i is not a prime number\n", num);
+    }
+    
 }
